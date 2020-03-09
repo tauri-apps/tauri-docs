5918d4867070a6e549d36730e85745392aed41b2
Laegel <valentin.chouaf@laposte.net>
2020-03-09 10:08:35 +0100

    Added Docusaurus files

---
+++ website/pages/en/api.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary.js');
+
+const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
+
+const translate = require('../../server/translate.js').translate;
+
+
+class API extends React.Component {
+  render() {
+
+    return (
+      <div className="docMainWrapper wrapper">
+        <Container className="mainContainer documentContainer postContainer">
+          
+        </Container>
+      </div>
+    );
+  }
+}
+
+API.defaultProps = {
+  language: 'en',
+};
+
+module.exports = API;
