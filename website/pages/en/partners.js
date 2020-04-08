6533647cd6eb53924e3176955fb2ec6230c4b4a8
Laegel <valentin.chouaf@laposte.net>
2020-04-08 10:49:41 +0200

    fix: Added minimal code so it can build

---
+++ website/pages/en/partners.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2017-present, FacePartners, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react')
+
+const CompLibrary = require('../../core/CompLibrary.js')
+
+const Container = CompLibrary.Container
+const GridBlock = CompLibrary.GridBlock
+
+const translate = require('../../server/translate.js').translate
+
+class Partners extends React.Component {
+  render() {
+
+    return (
+      <div className="docMainWrapper wrapper">
+
+      </div>
+    )
+  }
+}
+
+Partners.defaultProps = {
+  language: 'en',
+}
+
+module.exports = Partners
