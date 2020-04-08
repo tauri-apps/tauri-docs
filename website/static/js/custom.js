895d97ce6bed3d42a15747c2afdd0af8b22de651
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-04-08 14:55:58 -0400

    hide and reveal the documentation navbar on scroll

--- website/static/js/custom.js
+++ website/static/js/custom.js
@@ -18,4 +18,39 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   document.body.classList.toggle(theme)
+
+  // The debounce function receives updateScroll as a parameter
+  const debounce = (fn) => {
+    // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+    let frame;
+    // The debounce function returns a new function that can receive a variable number of arguments
+    return (...params) => {
+      // If the frame variable has been defined, clear it now, and queue for next frame
+      if (frame) {
+        cancelAnimationFrame(frame);
+      }
+      // Queue our function call for the next frame
+      frame = requestAnimationFrame(() => {
+        // Call our function and pass any params we received
+        fn(...params);
+      });
+    }
+  };
+
+  // Reads out the scroll position and stores it in the data attribute
+  // so we can use it in our stylesheets
+  const storeScroll = () => {
+    if (document.documentElement.dataset.scroll !== window.scrollY) {
+      if (window.scrollY > document.documentElement.dataset.scroll) {
+        document.documentElement.dataset.show = false;
+      } else {
+        document.documentElement.dataset.show = true;
+      }
+    }
+    document.documentElement.dataset.scroll = window.scrollY;
+  }
+  // Listen for new scroll events, here we debounce our `storeScroll` function
+  document.addEventListener('scroll', debounce(updateScroll), { passive: true });
+  // Update scroll position for first time
+  updateScroll();
 })
