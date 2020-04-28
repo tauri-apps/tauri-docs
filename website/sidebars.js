5e341d4f9db26ae8ef32a290bf7885983b77dbe0
Laegel <valentin.chouaf@laposte.net>
2020-04-28 23:31:23 +0200

    chore(structure): Reorganized sidebars

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,15 +1,36 @@
 module.exports = {
-  docs: {
-    'Getting started': ['intro', 'setup-linux', 'setup-macos', 'setup-windows'],
-    Usage: [
-      'usage',
-      'patterns',
-      'integration',
-      'development',
-      'publishing',
-      'identity',
-    ],
-    Tips: [],
-    API: ['config', 'js', 'rust'],
-  },
+  docs: [
+    {
+      label: 'Getting started',
+      type: 'category',
+      items: [
+        'getting-started/intro',
+        'getting-started/setup-linux',
+        'getting-started/setup-macos',
+        'getting-started/setup-windows',
+      ],
+    },
+    {
+      label: 'Usage',
+      type: 'category',
+      items: [
+        'usage/intro',
+        'usage/patterns',
+        'usage/integration',
+        'usage/development',
+        'usage/publishing',
+        'usage/identity',
+      ],
+    },
+    {
+      label: 'Tips',
+      type: 'category',
+      items: [],
+    },
+    {
+      label: 'API',
+      type: 'category',
+      items: ['config', 'js', 'rust'],
+    },
+  ],
 }
