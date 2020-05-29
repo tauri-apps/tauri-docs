fb8fe39e4726658eda3236335c9a62f8499008b3
Laegel <valentin.chouaf@laposte.net>
2020-05-29 23:10:36 +0200

    fix: Changed link & added icons (#54)
    * fix: Changed link & added icons
    
    * fix: moved icons out of link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,8 +14,11 @@ const siteConfig = {
   themeConfig: {
     announcementBar: {
       id: 'testapp',
-      content:
-        '🚀 Download our validation app to take Tauri for a test drive: <a href="https://github.com/tauri-apps/tauri/releases/download/0.5.2-binaries/app_0.1.0_amd64.deb" target="_blank">[Linux - deb]</a> - <a href="https://github.com/tauri-apps/tauri/releases/download/0.5.2-binaries/app-macos.dmg" target="_blank">[MacOS - dmg]</a> - <a href="https://github.com/tauri-apps/tauri/releases/download/0.5.2-binaries/app.exe" target="_blank">[Windows - exe]</a>',
+      content: `🚀 Download our validation app to take Tauri for a test drive: 
+       <br/> <i class="ti-linux"></i> <a href="https://github.com/tauri-apps/tauri/releases/download/0.5.2-binaries/app_0.1.0_amd64.deb" target="_blank">Linux - deb</a>
+           - <i class="ti-apple"></i> <a href="https://github.com/tauri-apps/tauri/releases/download/0.5.2-binaries/app-macos.dmg" target="_blank">MacOS - dmg</a>
+           - <i class="ti-microsoft"></i> <a href="https://github.com/tauri-apps/tauri/releases/download/0.5.2-binaries/app.x64.msi" target="_blank">Windows - msi</a>
+           - <i class="ti-github"></i> <a href="https://github.com/tauri-apps/tauri/tree/dev/tauri/examples/communication" target="_blank">Source</a>`,
     },
     navbar: {
       logo: {
