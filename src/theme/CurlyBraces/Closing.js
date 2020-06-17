cecf56ec29aac48104b2de743193857e5bb33bf5
Laegel <valentin.chouaf@laposte.net>
2020-06-17 17:18:24 +0200

    Feat/rework configuration (#58)
    * feat(style): Added some visual helpers
    
    * feat(docs): Reworking layout
    
    * feat(docs): Removed unexposed props
    
    * feat(styles): Improved layout
    
    * feat(docs): Adding osx, deb
    
    * feat(docs): Refining props
    
    * feat(docs): Added missing explanations
    
    * chore(config) add beforeDev/beforeBuild commands example
    
    * fix(styles): Minor style fix
    
    * feat(docs): Applying review comments
    
    * feat(docs): Listing category choices
    
    * feat(docs): Added optional hints
    
    * feat(style): Added layout components
    
    * fix(style): Added a small CSS fix
    
    * feat(docs): Filled descriptions
    
    * feat(docs): Added CLI param examples
    
    * Update config.md
    
    Co-authored-by: Lucas Fernandes Nogueira <lucasfernandesnog@gmail.com>

---
+++ src/theme/CurlyBraces/Closing.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+export default () => <span className={styles.closing}></span>
---
+++ src/theme/Properties/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+import { Opening, Closing } from '@theme/CurlyBraces'
+
+export default ({ rows }) => (
+  <div className={styles.row}>
+    <Opening />
+    {rows.map((row) => (
+      <div className={styles.content}>
+        <div
+          className={styles.property}
+          dangerouslySetInnerHTML={{
+            __html: `<strong><code>${row.property}</code></strong><span><code>${
+              row.optional ? '?' : ''
+            }: ${row.type}</code></span>`,
+          }}
+        ></div>
+        {!row.child && (
+          <div
+            className={styles.description}
+            dangerouslySetInnerHTML={{ __html: row.description }}
+          ></div>
+        )}
+
+        {row.child}
+      </div>
+    ))}
+    <Closing />
+  </div>
+)
