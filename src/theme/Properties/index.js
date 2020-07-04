32f698966cc1135da74fdb8454d9e445bcdae4e5
Lucas Nogueira <lucas@tauri.studio>
2020-07-04 00:31:51 -0300

    feat(docs) use rc-collapse on the config docs

--- src/theme/Properties/index.js
+++ src/theme/Properties/index.js
@@ -1,35 +1,56 @@
 import React from 'react'
 import classnames from 'classnames'
 import styles from './styles.module.css'
+import Collapse, { Panel } from 'rc-collapse'
 
 import { Opening, Closing } from '@theme/CurlyBraces'
 
-export default ({ rows, anchorRoot = '' }) => (
-  <div className={styles.row}>
-    <Opening />
-    {rows.map((row) => (
-      <div
-        id={(anchorRoot ? anchorRoot + '.' : '') + row.property}
-        className={classnames(styles.content, 'anchorify')}
-      >
-        <div
-          className={styles.property}
-          dangerouslySetInnerHTML={{
-            __html: `<code><strong>${row.property}</strong><span>${
-              row.optional ? '?' : ''
-            }: ${row.type}</span></code>`,
-          }}
-        ></div>
-        {!row.child && (
-          <div
-            className={styles.description}
-            dangerouslySetInnerHTML={{ __html: row.description }}
-          ></div>
-        )}
+function Header (row) {
+  return (
+    <div
+      className={styles.property}
+      dangerouslySetInnerHTML={{
+        __html: `<code><strong>${row.property}</strong><span>${
+          row.optional ? '?' : ''
+        }: ${row.type}</span></code>`,
+      }}
+    ></div>
+  )
+}
 
-        {row.child}
-      </div>
-    ))}
-    <Closing />
-  </div>
-)
+function Content (row) {
+  return (
+    <div>
+      {!row.child && (Header(row))}
+      {!row.child && (
+        <div
+          className={styles.description}
+          dangerouslySetInnerHTML={{ __html: row.description }}
+        ></div>
+      )}
+
+      {row.child}
+    </div>
+  )
+}
+
+export default ({ rows, anchorRoot = '' }) => {
+  return (
+    <div className={styles.row}>
+      <Opening />
+      <Collapse>
+        {rows.map((row) => (
+          row.child ? <Panel extra={Header(row)}>
+            {Content(row)}
+          </Panel> : <div
+            id={(anchorRoot ? anchorRoot + '.' : '') + row.property}
+            className={classnames(styles.content, 'anchorify')}
+          >
+            {Content(row)}
+          </div>
+        ))}
+      </Collapse>
+      <Closing />
+    </div>
+  )
+}
