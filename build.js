0e0876972562ef2358702ac571f2f096d7883ae5
Laegel <valentin.chouaf@laposte.net>
2020-07-20 12:49:30 +0200

    feat: Trying to make it work with the new setup on Netlify

---
+++ build.js
@@ -0,0 +1,26 @@
+const { execSync } = require('child_process')
+
+const languages = [
+  'en',
+  // 'fr',
+  // 'pt-br',
+  // 'zh-hant',
+  // 'zh-hans',
+  // 'ru'
+]
+languages.forEach(async (language) => {
+  console.log(`Building ${language} language...`)
+
+  // Trying to build concurrently made the generation crash, that's why we're using execSync here
+  execSync(
+    `LANGUAGE=${language} yarn build --out-dir ./build/${language}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`)
+        return
+      }
+      console.log(`stdout: ${stdout}`)
+      console.error(`stderr: ${stderr}`)
+    }
+  )
+})
