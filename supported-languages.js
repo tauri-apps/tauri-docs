b2c479878ac8ef75c27ce865201cef11917b0cd6
Laegel <valentin.chouaf@laposte.net>
2020-07-29 16:38:00 +0200

    fix: Addded country mapping to redirects

--- supported-languages.js
+++ supported-languages.js
@@ -1,6 +1,9 @@
+// Supported languages
+// Note that you'll need to modify netlify.toml to enable redirects!
+
 module.exports = {
   en: 'English',
-  fr: 'Français',
+  // fr: 'Français',
   // 'zh-hans': '简体中文',
   // 'pt-br': 'Português',
   // 'zh-hant': '繁體中文',
