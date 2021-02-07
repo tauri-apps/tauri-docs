8babd5933c4f6858b9203f0183b8a58496e4338c
Laegel <valentin.chouaf@laposte.net>
2021-02-07 15:06:04 +0100

    fix: Repairing build

--- src/theme/SearchBar.js
+++ src/theme/SearchBar.js
@@ -1,18 +1,18 @@
-import React, { useEffect } from 'react'
-import docsSearchBar from 'docs-searchbar.js'
+import React, {useEffect} from 'react'
 import 'docs-searchbar.js/dist/cdn/docs-searchbar.min.css'
 
 const SearchBar = () => {
   useEffect(() => {
+    const docsSearchBar = require('docs-searchbar.js').default
     docsSearchBar({
       hostUrl: 'https://demos.meilisearch.com',
       apiKey:
         'dc3fedaf922de8937fdea01f0a7d59557f1fd31832cb8440ce94231cfdde7f25',
       indexUid: 'steam-video-games',
       inputSelector: '#search-bar-input',
-      debug: process.env.NODE_ENV === "development", // Set debug to true if you want to inspect the dropdown
+      debug: process.env.NODE_ENV === 'development', // Set debug to true if you want to inspect the dropdown
       transformData: function (values) {
-        return values.map((value) => ({ lvl0: value.name, lvl1: "" }))
+        return values.map((value) => ({ lvl0: value.name, lvl1: '' }))
       },
     })
   }, [])
