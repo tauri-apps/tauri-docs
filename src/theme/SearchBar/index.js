92095a788d0e3d48a8f1574b0f3a3f25eb984817
Laegel <valentin.chouaf@laposte.net>
2021-02-07 13:59:26 +0100

    feat(search): Added missing files

---
+++ src/theme/SearchBar/index.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import docsSearchBar from 'docs-searchbar.js'
+import 'docs-searchbar.js/dist/cdn/docs-searchbar.min.css'
+
+const SearchBar = () => {
+  useEffect(() => {
+    docsSearchBar({
+      hostUrl: 'https://demos.meilisearch.com',
+      apiKey:
+        'dc3fedaf922de8937fdea01f0a7d59557f1fd31832cb8440ce94231cfdde7f25',
+      indexUid: 'steam-video-games',
+      inputSelector: '#search-bar-input',
+      debug: process.env.NODE_ENV === "development", // Set debug to true if you want to inspect the dropdown
+      transformData: function (values) {
+        return values.map((value) => ({ lvl0: value.name, lvl1: "" }))
+      },
+    })
+  }, [])
+  return (
+    <div>
+      <input type="search" id="search-bar-input" />
+    </div>
+  )
+}
+
+export default SearchBar
