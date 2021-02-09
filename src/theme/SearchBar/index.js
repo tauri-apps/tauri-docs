4ab16c60e6f6f35126b1ca3544af1da6d237da29
Laegel <valentin.chouaf@laposte.net>
2021-02-09 11:58:19 +0100

    feat(searchbar)

---
+++ src/theme/SearchBar/index.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import classNames from 'classnames'
+import 'docs-searchbar.js/dist/cdn/docs-searchbar.min.css'
+import styles from './searchbar.module.css'
+
+const SearchBar = () => {
+  useEffect(() => {
+    const docsSearchBar = require('docs-searchbar.js').default
+    docsSearchBar({
+      hostUrl: 'https://search.tauri.studio',
+      apiKey:
+        'ea0105f56bb5a2111ed28c7a0c637fc0bed07273f571dc7cb1f73900e44f8e7f',
+      indexUid: '0_0_0',
+      inputSelector: '#search-bar-input',
+      debug: process.env.NODE_ENV === 'development',
+      transformData(hits) {
+        return hits.map((hit) => ({ ...hit, url: '/en/docs/' + hit.url }))
+      },
+    })
+  }, [])
+  return (
+    <div class="search-box">
+      <input
+        type="text"
+        class={classNames(styles.search)}
+        id="search-bar-input"
+      />
+    </div>
+  )
+}
+
+export default SearchBar
