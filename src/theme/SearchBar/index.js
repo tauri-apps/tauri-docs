e943418f3be9e2ed4fdeb78f3114e0a2599f931b
Laegel <valentin.chouaf@laposte.net>
2021-04-24 10:21:51 +0200

    chore: Upgraded version index (#192)

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -11,7 +11,7 @@ const SearchBar = ({className}) => {
       hostUrl: 'https://search.tauri.studio',
       apiKey:
         'ea0105f56bb5a2111ed28c7a0c637fc0bed07273f571dc7cb1f73900e44f8e7f',
-      indexUid: '0_0_0',
+      indexUid: '1_0_0_beta_rc_2',
       inputSelector: '#search-bar-input',
       debug: process.env.NODE_ENV === 'development',
       transformData(hits) {
