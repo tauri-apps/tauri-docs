c8b15bcf34f3b8ef4e5fd78f56c8ed99e94b2cf2
Laegel <valentin.chouaf@laposte.net>
2021-05-29 16:09:21 +0200

    feat: Added version file

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react'
 import classNames from 'classnames'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import 'docs-searchbar.js/dist/cdn/docs-searchbar.min.css'
 import styles from './searchbar.module.css'
 import Icon from '@theme/Icon'
 
 const SearchBar = ({className}) => {
+  const context = useDocusaurusContext()
+  const { siteConfig = {} } = context
+
   useEffect(() => {
     const docsSearchBar = require('docs-searchbar.js').default
     docsSearchBar({
       hostUrl: 'https://search.tauri.studio',
       apiKey:
         'ea0105f56bb5a2111ed28c7a0c637fc0bed07273f571dc7cb1f73900e44f8e7f',
-      indexUid: '1_0_0_beta_rc_2',
+      indexUid: siteConfig.themeConfig.version.replace(/\W/g, '_'),
       inputSelector: '#search-bar-input',
       debug: process.env.NODE_ENV === 'development',
       transformData(hits) {
