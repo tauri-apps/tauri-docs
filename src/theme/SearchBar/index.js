e7c1ee78a8ee5b1a5369af47876d070e91ab58e8
Laegel <valentin.chouaf@laposte.net>
2021-05-30 11:15:49 +0200

    fix: Trim on version to avoid linebreaks

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -15,7 +15,7 @@ const SearchBar = ({className}) => {
       hostUrl: 'https://search.tauri.studio',
       apiKey:
         'ea0105f56bb5a2111ed28c7a0c637fc0bed07273f571dc7cb1f73900e44f8e7f',
-      indexUid: siteConfig.themeConfig.version.replace(/\W/g, '_'),
+      indexUid: siteConfig.themeConfig.version.trim().replace(/\W/g, '_'),
       inputSelector: '#search-bar-input',
       debug: process.env.NODE_ENV === 'development',
       transformData(hits) {
