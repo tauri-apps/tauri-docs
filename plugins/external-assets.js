446ebf93c44d16fbd97f2345b987c715f43487f1
Alve Larsson <larsalvelarsson@gmail.com>
2021-08-26 19:40:38 +0200

    Add image metadata

--- plugins/external-assets.js
+++ plugins/external-assets.js
@@ -9,6 +9,41 @@ module.exports = (context, options) => ({
         //     src: context.baseUrl + 'js/ethicalads.min.js',
         //   },
         // },
+        {
+          tagName: 'meta',
+          attributes: {
+            property: 'og:type',
+            content: 'website'
+          },
+        },
+        {
+          tagName: 'meta',
+          attributes: {
+            property: 'og:url',
+            content: context.baseUrl
+          },
+        },
+        {
+          tagName: 'meta',
+          attributes: {
+            property: 'og:image',
+            content: context.baseUrl + '/img/social.jpg'
+          },
+        },
+        {
+          tagName: 'meta',
+          attributes: {
+            property: 'twitter:card',
+            content: 'summary_large_image'
+          },
+        },
+        {
+          tagName: 'meta',
+          attributes: {
+            property: 'twitter:image',
+            content: context.baseUrl + '/img/social.jpg'
+          },
+        },
       ],
     }
   },
