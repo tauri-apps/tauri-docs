fd2a93170a727403ae08e0be3f2cb27b36d251ad
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-01-21 18:42:15 +0000

    Update footer and logo for light mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -172,13 +172,13 @@ const siteConfig = {
       hideOnScroll: false,
       logo: {
         alt: 'Tauri Logo',
-        src: 'img/tauri_with_wordmark.svg',
+        src: 'img/tauri_with_wordmark_dark.svg',
+        srcDark: 'img/tauri_with_wordmark.svg'
       },
       items: navbarItems,
     },
 
     footer: {
-      style: 'dark',
       links: footerLinks,
       copyright: `Copyright © ${new Date().getFullYear()} Tauri Contributors. CC-BY / MIT`,
     },
