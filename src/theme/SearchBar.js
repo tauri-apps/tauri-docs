7ee1e5f5ffdb757cb7be7d97b996a36afc46169a
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-01-22 14:15:46 +0000

    Toggle searchbar based on theme

--- src/theme/SearchBar.js
+++ src/theme/SearchBar.js
@@ -2,9 +2,11 @@ import React, { useEffect } from 'react'
 import classNames from 'classnames'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import 'docs-searchbar.js/dist/cdn/docs-searchbar.min.css'
+import useThemeContext from '@theme/hooks/useThemeContext'
 
 const SearchBar = () => {
   const context = useDocusaurusContext()
+  const { isDarkTheme } = useThemeContext()
   const { siteConfig = {} } = context
 
   useEffect(() => {
@@ -15,7 +17,7 @@ const SearchBar = () => {
         'ea0105f56bb5a2111ed28c7a0c637fc0bed07273f571dc7cb1f73900e44f8e7f',
       indexUid: siteConfig.themeConfig.version.trim().replace(/\W/g, '_'),
       inputSelector: '#search-bar-input',
-      enableDarkMode: 'auto'
+      enableDarkMode: isDarkTheme
     })
   }, [])
   return (
