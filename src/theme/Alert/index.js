076e6e36d49bc8220addaa13ca092be22511e3cb
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-01-22 11:12:25 +0000

    Add line break after Alert

--- src/theme/Alert/index.js
+++ src/theme/Alert/index.js
@@ -1,21 +1,22 @@
 import React from 'react'
 
 const types = {
-  primary: "",
-  secondary: "",
-  success: "check-circle-fill",
-  info: "info-circle-fill",
-  warning: "exclamation-circle-fill",
-  danger: "x-octagon-fill"
+  primary: '',
+  secondary: '',
+  success: 'check-circle-fill',
+  info: 'info-circle-fill',
+  warning: 'exclamation-circle-fill',
+  danger: 'x-octagon-fill',
 }
 
 export default ({ type = 'info', title, children }) => (
-  <div class={`alert alert--${type}`} role="alert">
-    <strong>
-      <i class={`bi bi-${types[type]}`}/> {title}
-    </strong>
-    <div>
-      {children}
+  <div>
+    <div class={`alert alert--${type}`} role="alert">
+      <strong>
+        <i class={`bi bi-${types[type]}`} /> {title}
+      </strong>
+      <div>{children}</div>
     </div>
+    <br />
   </div>
 )
