4bcdc3d52fdcf3af5a3c4bc52f723bfb215d7a25
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-02-07 21:09:33 +0000

    Update API key

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -13,10 +13,10 @@ const SearchBar = () => {
     docsSearchBar({
       hostUrl: 'https://search2.tauri.studio',
       apiKey:
-        'ea0105f56bb5a2111ed28c7a0c637fc0bed07273f571dc7cb1f73900e44f8e7f',
+        'XZEH8BS90ee09c45215a8421c06857bcbde5c1a6797bdf4859a57a3ac1228a2b81df0994',
       indexUid: 'consolidated',
       inputSelector: '#search-bar-input',
-      enableDarkMode: isDarkTheme
+      enableDarkMode: isDarkTheme,
     })
   }, [isDarkTheme])
 
