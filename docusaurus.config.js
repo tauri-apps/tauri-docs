6d5735fd0423716d5b6cbe8f1f655986d842ef0b
Jonas Kruckenberg <iterpre@protonmail.com>
2022-02-08 12:25:16 +0100

    feat: remove showcase section (#399)
    * feat: remove showcase section
    
    * feat: add awesome tauri link to navbar/footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,6 +6,7 @@ const version = fs.readFileSync('./version.txt', 'utf-8').trim()
 const repoUrl = 'https://github.com/tauri-apps/tauri'
 const discordUrl = 'https://discord.com/invite/tauri'
 const devToUrl = 'https://dev.to/tauri'
+const awesomeTauriUrl = 'https://github.com/tauri-apps/awesome-tauri'
 
 const navbarItems = [
   {
@@ -37,13 +38,13 @@ const navbarItems = [
         to: 'docs/community/contributor-guide',
       },
       {
-        label: 'Showcase',
-        to: 'showcase',
-      },
-      {
         label: 'Discord',
         href: discordUrl,
       },
+      {
+        label: 'Awesome Tauri',
+        href: awesomeTauriUrl
+      }
     ],
   },
   {
@@ -94,10 +95,6 @@ const footerLinks = [
         href: 'mailto:contact@tauri.studio',
       },
       {
-        label: 'Discord',
-        href: discordUrl,
-      },
-      {
         label: 'Twitter',
         href: 'https://twitter.com/TauriApps',
       },
@@ -124,9 +121,13 @@ const footerLinks = [
     title: 'Community',
     items: [
       {
-        label: 'Showcase',
-        to: 'showcase',
+        label: 'Discord',
+        href: discordUrl,
       },
+      {
+        label: 'Awesome Tauri',
+        href: awesomeTauriUrl
+      }
     ],
   },
 ]
