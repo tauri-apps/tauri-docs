be5f82b8bfe300fe6e42a1b4541e58831ef9d69a
Hyunseung Lee <hyunseunglee2008@gmail.com>
2022-05-07 13:30:27 +0900

    Update Release Candidate Announcement Banner Style
    There were 10px of margin on top. Removed it to make it look better.
    In mobiles below 500px, the link went to the next line. Made it move to the bottom in mobile.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -175,7 +175,7 @@ const siteConfig = {
     announcementBar: {
       id: 'rc',
       content:
-        "<div id='announcement-rc'>🚀 Tauri Release Candidate has landed! <a target='_blank' rel='noopener noreferrer' href='https://dev.to/tauri/tauri-10-release-candidate-53jk'>Click here for more details.</a></div>",
+        "<div id='announcement-rc'>🚀 Tauri Release Candidate has landed! <a id='announcement-link' target='_blank' rel='noopener noreferrer' href='https://dev.to/tauri/tauri-10-release-candidate-53jk'>Click here for more details.</a></div>",
       backgroundColor: 'var(--ifm-color-primary)',
     },
     navbar: {
