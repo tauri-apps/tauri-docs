9f5f5fd451dd37f5b088ff8a41cf61a913033f6d
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-05-07 13:56:11 +0100

    Add textColor to improve contrast

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -177,6 +177,7 @@ const siteConfig = {
       content:
         "<div id='announcement-rc'>🚀 Tauri Release Candidate has landed! <a id='announcement-link' target='_blank' rel='noopener noreferrer' href='https://dev.to/tauri/tauri-10-release-candidate-53jk'>Click here for more details.</a></div>",
       backgroundColor: 'var(--ifm-color-primary)',
+      textColor: 'var(--ifm-button-color)',
     },
     navbar: {
       hideOnScroll: false,
