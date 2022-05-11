cc4f485e89b15d27b5c68aa0bf5f2535c956f718
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-05-11 12:54:59 +0100

    Fix script to get the last version, not current

--- src/utils/scripts.js
+++ src/utils/scripts.js
@@ -5,7 +5,7 @@ export function getCurrentVersion() {
   try {
     return context.globalData[
       'docusaurus-plugin-content-docs'
-    ].default.versions.filter((item) => item.name == 'current')[0].path
+    ].default.versions.filter((item) => item.isLast)[0].path
   } catch {
     return 'v1'
   }
