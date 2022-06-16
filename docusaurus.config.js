dd995a0b5798a9403ed6e80c317790e101ebc909
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-06-16 18:27:37 +0200

    Update tauri domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -134,7 +134,7 @@ const footerLinks = [
     items: [
       {
         label: 'Mail',
-        href: 'mailto:contact@tauri.studio',
+        href: 'mailto:contact@tauri.app',
       },
       {
         label: 'Twitter',
@@ -204,7 +204,7 @@ const siteConfig = {
     },
     announcementBar: {
       content:
-        "<b>🚀 Tauri 1.0 has landed! <a href='https://tauri.studio'>Click here for more details</a></b>",
+        "<b>🚀 Tauri 1.0 has landed! <a href='https://tauri.app'>Click here for more details</a></b>",
       backgroundColor: 'var(--ifm-color-primary)',
       textColor: 'var(--ifm-button-color)',
     },
