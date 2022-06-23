ab2a0317d7759c6995b0948f590a1f75aab31b68
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-06-23 18:47:44 +0100

    Switch to esbuild

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -326,6 +326,17 @@ const siteConfig = {
       },
     ],
   ],
+
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve('esbuild-loader'),
+      options: {
+        loader: 'tsx',
+        format: isServer ? 'cjs' : undefined,
+        target: isServer ? 'node12' : 'es2017',
+      },
+    }),
+  },
 }
 
 module.exports = siteConfig
