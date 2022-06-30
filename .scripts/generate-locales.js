a7c7e0269df22f8c566035f73433aa070b441eba
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-06-30 14:55:00 +0100

    Add environment

--- .scripts/generate-locales.js
+++ .scripts/generate-locales.js
@@ -2,9 +2,8 @@ const fs = require('fs')
 const core = require('@actions/core')
 
 try {
-  //   var locales = fs.readdirSync('i18n')
-  //   console.log(`Found the locales ${locals}`)
-  var locales = ['en', 'fr', 'es', 'zh']
+  var locales = fs.readdirSync('i18n')
+  console.log(`Found the locales ${locals}`)
   core.setOutput('language-list', JSON.stringify(locales))
 } catch {
   core.setFailed
