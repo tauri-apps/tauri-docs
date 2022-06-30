805600d26007167e1c845b45d9f3b01ded22cba7
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-06-30 15:13:52 +0100

    Fix typo

--- .scripts/generate-locales.js
+++ .scripts/generate-locales.js
@@ -3,7 +3,7 @@ const core = require('@actions/core')
 
 try {
   var locales = fs.readdirSync('i18n')
-  console.log(`Found the locales ${locals}`)
+  console.log(`Found the locales ${locales}`)
   core.setOutput('language-list', JSON.stringify(locales))
 } catch (error) {
   core.setFailed(`An issue ocurred while retrieving the languages: ${error}`)
