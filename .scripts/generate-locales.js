d4299bcd36798741210916cc34a13da0f1646061
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-06-30 14:25:04 +0100

    Point to script

--- .scripts/generate-locales.js
+++ .scripts/generate-locales.js
@@ -2,8 +2,9 @@ const fs = require('fs')
 const core = require('@actions/core')
 
 try {
-  var locales = fs.readdirSync('i18n')
-  console.log(`Found the locales ${locals}`)
+  //   var locales = fs.readdirSync('i18n')
+  //   console.log(`Found the locales ${locals}`)
+  var locales = ['en', 'fr', 'es', 'zh']
   core.setOutput(JSON.stringify(locales))
 } catch {
   core.setFailed
