7fb83bba04d574c8cb3d1fdfa954b2c0a4300f4c
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-06-30 18:48:17 +0100

    Add cache loop

--- .scripts/ci-postbuild.js
+++ .scripts/ci-postbuild.js
@@ -1,8 +1,14 @@
 const fs = require('fs')
 const core = require('@actions/core')
+const tc = require('@actions/cache')
 
 try {
-  console.log('I ran!')
+  var locales = core.getInput('locale-list', { required: true })
+  // Get the locales variable
+  // Load in each via a loop
+  // Merge them together into the build folder
+  // Tell netlify to pick it up (outside of this file and with another action probably)
+  console.log('I ran!', locales)
 } catch (error) {
-  core.setFailed(`An issue ocurred while retrieving the languages: ${error}`)
+  core.setFailed(`An issue ocurred while combining locales: ${error}`)
 }
