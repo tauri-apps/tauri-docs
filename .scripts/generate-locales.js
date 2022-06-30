52bc4c8013389b46d38a0396642418d5e6c6d3d7
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-06-30 15:34:37 +0100

    Switch to dynamci

--- .scripts/generate-locales.js
+++ .scripts/generate-locales.js
@@ -2,8 +2,7 @@ const fs = require('fs')
 const core = require('@actions/core')
 
 try {
-  // var locales = fs.readdirSync('i18n')
-  var locales = ['en', 'fr', 'es']
+  var locales = fs.readdirSync('i18n')
   console.log(`Found the locales ${locales}`)
   core.setOutput('locale-list', locales)
 } catch (error) {
