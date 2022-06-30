744ea5425f472a893d1ba15bf7b6d2f1077cbf74
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-06-30 15:29:51 +0100

    Experiementing with node fun

--- .scripts/generate-locales.js
+++ .scripts/generate-locales.js
@@ -2,7 +2,8 @@ const fs = require('fs')
 const core = require('@actions/core')
 
 try {
-  var locales = fs.readdirSync('i18n')
+  // var locales = fs.readdirSync('i18n')
+  var locales = ['en', 'fr', 'es']
   console.log(`Found the locales ${locales}`)
   core.setOutput('language-list', locales)
 } catch (error) {
