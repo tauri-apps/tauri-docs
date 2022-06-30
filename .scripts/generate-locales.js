569391038b0bc4fb256dd4b484134a71deb18261
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-06-30 15:16:17 +0100

    Don't stringify

--- .scripts/generate-locales.js
+++ .scripts/generate-locales.js
@@ -4,7 +4,7 @@ const core = require('@actions/core')
 try {
   var locales = fs.readdirSync('i18n')
   console.log(`Found the locales ${locales}`)
-  core.setOutput('language-list', JSON.stringify(locales))
+  core.setOutput('language-list', locales)
 } catch (error) {
   core.setFailed(`An issue ocurred while retrieving the languages: ${error}`)
 }
