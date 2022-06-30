fa1b1607acc06f4d506c03faaa1948f6482ee79c
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-06-30 23:34:51 +0100

    Read in caches

--- .scripts/ci-postbuild.js
+++ .scripts/ci-postbuild.js
@@ -1,15 +1,25 @@
 const fs = require('fs')
 const core = require('@actions/core')
-const tc = require('@actions/cache')
+const cache = require('@actions/cache')
 
 try {
-  var locales = process.env.LOCALES
-  // core.getInput('locale-list', { required: true })
+  const locales = process.env.LOCALES
+
+  for (locale in locales) {
+    const path = 'build/' + locale
+    const key = `${locale}-build`
+    const cacheKey = await cache.restoreCache(path, key)
+
+    console.log(path, key, cacheKey)
+
+    if (!cacheKey) {
+      throw `Cache couldn't be restored for locale ${locale}`
+    }
+  }
   // Get the locales variable
   // Load in each via a loop
   // Merge them together into the build folder
   // Tell netlify to pick it up (outside of this file and with another action probably)
-  console.log(`I ran! Locales: ${locales}`)
 } catch (error) {
-  core.setFailed(`An issue ocurred while combining locales: ${error}`)
+  core.setFailed(`An issue ocurred while combining locale builds: ${error}`)
 }
