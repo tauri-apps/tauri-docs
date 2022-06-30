d5888a9853e7ec28cabfd62be714d64a60d3a71a
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-06-30 15:32:52 +0100

    Change to locale

--- .scripts/generate-locales.js
+++ .scripts/generate-locales.js
@@ -5,7 +5,7 @@ try {
   // var locales = fs.readdirSync('i18n')
   var locales = ['en', 'fr', 'es']
   console.log(`Found the locales ${locales}`)
-  core.setOutput('language-list', locales)
+  core.setOutput('locale-list', locales)
 } catch (error) {
   core.setFailed(`An issue ocurred while retrieving the languages: ${error}`)
 }
