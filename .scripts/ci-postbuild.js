6480ffd187e86aefe5dc77f92476795863f5b3f1
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-07-01 02:21:56 +0100

    Remove try catch

--- .scripts/ci-postbuild.js
+++ .scripts/ci-postbuild.js
@@ -2,37 +2,28 @@ const fs = require('fs')
 const core = require('@actions/core')
 const cache = require('@actions/cache')
 
-try {
-  const locales = JSON.parse(process.env.LOCALES)
-  console.log('Is it an array?', Array.isArray(locales))
+const locales = JSON.parse(process.env.LOCALES)
 
-  if (locales.length == 0) {
-    throw 'No locales were given to be built'
-  }
+if (locales.length == 0) {
+  throw 'No locales were given to be built'
+}
 
-  console.log(`Retrieved the following locales: ${locales}`)
+console.log(`Retrieved the following locales: ${locales}`)
 
-  for (locale of locales) {
-    console.log(`Started processing ${locale}`)
+for (locale of locales) {
+  console.log(`Started processing ${locale}`)
 
-    const path = 'build/' + locale
-    const key = `${locale}-build`
+  const path = 'build/' + locale
+  const key = `${locale}-build`
 
-    cache
-      .restoreCache(path, key)
-      .then((cacheKey) => {
-        console.log(path, key, cacheKey)
+  cache
+    .restoreCache(path, key)
+    .then((value) => {
+      console.log('Value is', value)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
 
-        if (!cacheKey) {
-          throw `Cache couldn't be restored for locale ${locale}`
-        }
-
-        console.log(`Finished processing ${locale}`)
-      })
-      .catch((error) => {
-        throw `Error ocurred while retrieving the cache: ${error}`
-      })
-  }
-} catch (error) {
-  core.setFailed(`An issue ocurred while combining locale builds: ${error}`)
+  console.log(cacheKey)
 }
