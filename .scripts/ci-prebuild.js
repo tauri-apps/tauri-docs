326c259ee604c8b2430aa679d14b9b08be6f6bf6
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-07-01 15:09:22 +0100

    Add in special i18n config

--- .scripts/ci-prebuild.js
+++ .scripts/ci-prebuild.js
@@ -4,6 +4,18 @@ const core = require('@actions/core')
 try {
   var locales = fs.readdirSync('i18n')
   console.log(`Found the locales ${locales}`)
+
+  var config = fs
+    .readFileSync(process.cwd() + '/docusaurus.config.js')
+    .toString()
+
+  config = config.replace(/(?<=locales: )(.*)(?=,)/, JSON.stringify(locales))
+  const outputFile = process.cwd() + '/docusaurus.config.i18n.js'
+
+  fs.writeFileSync(outputFile, config)
+
+  console.log(`Output file: ${outputFile}`)
+
   core.setOutput('locale-list', locales)
 } catch (error) {
   core.setFailed(`An issue ocurred while retrieving the languages: ${error}`)
