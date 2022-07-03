494b8e649353faa81731628046a8d822e25e4366
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-07-03 21:43:18 +0100

    Update build script

--- .scripts/build.js
+++ .scripts/build.js
@@ -1,5 +1,34 @@
+const fs = require('fs')
+const core = require('@actions/core')
+
 async function run() {
-  console.log('Regular build')
+  try {
+    console.log('Env:', process.env.I18N_BUILD)
+    var locales
+    var configFile = process.cwd() + '/docusaurus.config.js'
+    var config = fs.readFileSync(configFile).toString()
+
+    if (process.env.I18N_BUILD) {
+      console.log('Running an i18n build...')
+      locales = fs.readdirSync('i18n')
+
+      config = config.replace(
+        /(?<=locales: )(.*)(?=,)/,
+        JSON.stringify(locales)
+      )
+
+      console.log('Writing updated i18n config...')
+      fs.writeFileSync(configFile, config)
+      console.log('i18n config saved.')
+    } else {
+      console.log('Running a production deploy build...')
+      locales = config.match(/(?<=locales: )(.*)(?=,)/)
+    }
+    console.log(`Found the locales ${locales}`)
+    core.setOutput('locale-list', locales)
+  } catch (error) {
+    core.setFailed(`An issue ocurred while retrieving the languages: ${error}`)
+  }
 }
 
 run()
