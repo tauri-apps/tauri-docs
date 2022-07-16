721c5cea3e546cde7c45f33c72dc370bd2e5ae04
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-07-17 00:07:56 +0100

    Test updated deploy script (#778)
    * Test updated deploy script
    
    * Add check if internal PR
    
    * Fix github context
    
    * Test logging
    
    * Check if trusted PR
    
    * Additional logging
    
    * Chagne event type
    
    * log out head
    
    * Break apart logging
    
    * Move up a level
    
    * Move logging up
    
    * fix type
    
    * Remove logging
    
    * Update build logic
    
    * Run locale test for all builds

--- .scripts/build.js
+++ .scripts/build.js
@@ -8,18 +8,12 @@ async function run() {
     var configFile = process.cwd() + '/docusaurus.config.js'
     var config = fs.readFileSync(configFile).toString()
 
-    if (github.context.eventName === 'push') {
-      console.log('Running a production deploy build...')
-      var locales = config.match(/(?<=locales: )(.*)(?=,)/g)
-      locales = locales[0].replaceAll("'", '"')
-      locales = JSON.parse(locales)
+    var locales = config.match(/(?<=locales: )(.*)(?=,)/g)
+    locales = locales[0].replaceAll("'", '"')
+    locales = JSON.parse(locales)
 
-      console.log(`Found the locales ${locales}`)
-      core.setOutput('locale-list', locales)
-    } else {
-      console.log('Running a deploy preview...')
-      core.setOutput('locale-list', ['en'])
-    }
+    console.log(`Found the locales ${locales}`)
+    core.setOutput('locale-list', locales)
   } catch (error) {
     core.setFailed(`An issue ocurred while retrieving the languages: ${error}`)
   }
