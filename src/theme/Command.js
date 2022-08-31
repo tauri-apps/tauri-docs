9dd74178ff7a9356b404de2e17bfb76119c79511
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-08-31 20:25:27 +0100

    Remove wget, capitalisation updates

--- src/theme/Command.js
+++ src/theme/Command.js
@@ -5,10 +5,10 @@ import Tabs from '@theme/Tabs'
 import TabItem from '@theme/TabItem'
 
 const types = [
-  { label: 'npm', value: 'npm run tauri ' },
-  { label: 'yarn', value: 'yarn tauri ' },
-  { label: 'pnpm', value: 'pnpm tauri ' },
-  { label: 'cargo', value: 'cargo tauri ' },
+  { label: 'NPM', value: 'npm run tauri ' },
+  { label: 'Yarn', value: 'yarn tauri ' },
+  { label: 'PNPM', value: 'pnpm tauri ' },
+  { label: 'Cargo', value: 'cargo tauri ' },
 ]
 
 function insertDashDashBeforeOption(value, name) {
@@ -21,37 +21,32 @@ function insertDashDashBeforeOption(value, name) {
 
 export const CreateTauriApp = () => {
   return (
-    <Tabs>
-      <TabItem value="bash">
-        <CodeBlock title='curl' className={`language-shell`}>
+    <Tabs className="">
+      <TabItem value="Bash">
+        <CodeBlock className={`language-shell`}>
           {`sh <(curl https://create.tauri.app/sh)`}
         </CodeBlock>
-        <CodeBlock title='wget' className={`language-shell`}>
-          {`sh <(wget https://create.tauri.app/sh)`}
-        </CodeBlock>
       </TabItem>
-      <TabItem value="powershell">
+      <TabItem value="PowerShell">
         <CodeBlock className={`language-powershell`}>
           iwr https://create.tauri.app/ps | iex
         </CodeBlock>
       </TabItem>
-      <TabItem value="cargo">
+      <TabItem value="Cargo">
         <CodeBlock className={`language-shell`}>
           {`cargo install create tauri-app
 cargo create-tauri-app`}
         </CodeBlock>
       </TabItem>
-      <TabItem value="npm">
-        <CodeBlock className={`language-shell`}>
-          npm create tauri-app
-        </CodeBlock>
+      <TabItem value="NPM">
+        <CodeBlock className={`language-shell`}>npm create tauri-app</CodeBlock>
       </TabItem>
-      <TabItem value="yarn">
+      <TabItem value="Yarn">
         <CodeBlock className={`language-shell`}>
           yarn create tauri-app
         </CodeBlock>
       </TabItem>
-      <TabItem value="pnpm">
+      <TabItem value="PNPM">
         <CodeBlock className={`language-shell`}>
           pnpm create tauri-app
         </CodeBlock>
@@ -66,7 +61,7 @@ export default ({ name, shell = 'shell' }) => {
       {types.map((type) => (
         <TabItem value={type.value}>
           <CodeBlock className={`language-${shell}`}>
-            {type.label !== 'npm'
+            {type.label !== 'NPM'
               ? type.value + name
               : insertDashDashBeforeOption(type.value, name)}
           </CodeBlock>
