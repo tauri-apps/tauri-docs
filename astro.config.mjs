57d462a0474635e28a920fc1602437c3b3f3003d
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-12-28 12:58:05 +0000

    Add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    contentCollections: true,
+    contentCollections: true
   },
-})
+  integrations: [prefetch()]
+});
