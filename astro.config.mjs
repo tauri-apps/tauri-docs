ed174593f827c20b554a0454669bd1e8bf1284d3
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-12-30 14:49:17 +0000

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config'
-
 import prefetch from '@astrojs/prefetch'
 import tailwind from '@astrojs/tailwind'
 import Unocss from 'unocss/astro'
 import presetIcons from '@unocss/preset-icons'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tauri.app',
   experimental: {
     contentCollections: true,
   },
@@ -16,5 +17,6 @@ export default defineConfig({
     Unocss({
       presets: [presetIcons()],
     }),
+    sitemap(),
   ],
 })
