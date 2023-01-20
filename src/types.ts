744a993ff99780beb9c9054904d82f1e8777b255
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-01-20 18:06:31 +0000

    Remove unused types

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,3 @@
-import type { MarkdownHeading } from 'astro'
-
-export type Heading = MarkdownHeading
-
-export type BlogMetadata = {}
-
-export type Page = {
-  title: string
-  description?: string
-  image?: string
-  darkImage?: string
-  href: string
-}
-
 export type SidebarItem = {
   // If no title is specified then the first heading's text of a page should be used
   title?: string
