f708fa303bcc9a9590acbc3a647af1e5f8fa5f89
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-01-20 18:02:10 +0000

    Initial logic for sidebar

---
+++ src/content/docs-core-1/_sidebar.ts
@@ -0,0 +1,16 @@
+import type { SidebarItem } from '../../types'
+
+// TODO: Build out this sidebar object
+const sidebar: SidebarItem[] = [
+  {
+    title: 'Getting Started',
+    children: [{ title: 'HTML, CSS, and JavaScript' }],
+  },
+  { title: 'Development' },
+  {
+    title: 'FAQ',
+    id: 'faq.md',
+  },
+]
+
+export default sidebar
--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,11 @@ export type Page = {
   darkImage?: string
   href: string
 }
+
+export type SidebarItem = {
+  // If no title is specified then the first heading's text of a page should be used
+  title?: string
+  // The ID returned by `getCollection()` and `getEntry()`
+  id?: string
+  children?: [SidebarItem]
+}
