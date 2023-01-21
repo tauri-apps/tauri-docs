5d68bc0b88cfe09c7f4546c15961ff1befc1d8a4
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-01-21 20:58:11 +0000

    First pass at navbar styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,12 @@ const { fontFamily } = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    colors: {
+      'base-100': 'rgb(var(--color-base-100) / <alpha-value>)',
+      'base-200': 'rgb(var(--color-base-200) / <alpha-value>)',
+      'base-300': 'rgb(var(--color-base-300) / <alpha-value>)',
+      'base-content': 'rgb(var(--color-base-content) / <alpha-value>)',
+    },
     extend: {
       fontFamily: {
         ...fontFamily,
