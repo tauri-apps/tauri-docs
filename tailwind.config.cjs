54e93131c8690780ae9aed0a2812f49fb344c3bf
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-01-22 22:16:22 +0000

    Start button and navbar components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,18 +4,22 @@ const { fontFamily } = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    borderRadius: {
+      DEFAULT: '6px',
+    },
     colors: {
       'base-100': 'rgb(var(--color-base-100) / <alpha-value>)',
       'base-200': 'rgb(var(--color-base-200) / <alpha-value>)',
       'base-300': 'rgb(var(--color-base-300) / <alpha-value>)',
       'base-content': 'rgb(var(--color-base-content) / <alpha-value>)',
+      primary: 'rgb(var(--color-primary) / <alpha-value>)',
       border: 'rgba(var(--color-border))',
     },
     extend: {
       fontFamily: {
-        ...fontFamily,
         sans: ['Rubik', ...fontFamily.sans],
         mono: ['IBM Plex Mono', ...fontFamily.mono],
+        ...fontFamily,
       },
     },
   },
