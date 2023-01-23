b97f847d209217e893b27cb0fbd87424360a9ddc
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-01-23 17:56:07 +0000

    Feat/docs 2.0 tailwind (#1073)
    * Remove extra tailwind and daisyUI packages
    
    * Remove old tailwind plugins, cleanup config
    
    * First pass at navbar styling
    
    * Add divider styling
    
    * Add border color and water down search box
    
    * Cleanup index
    
    * Refactor navbar
    
    * Start button and navbar components
    
    * Add padding to headings
    
    * Add padding to p tag

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,62 +4,23 @@ const { fontFamily } = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    borderRadius: {
+      DEFAULT: '6px',
+    },
+    colors: {
+      'base-100': 'rgb(var(--color-base-100) / <alpha-value>)',
+      'base-200': 'rgb(var(--color-base-200) / <alpha-value>)',
+      'base-300': 'rgb(var(--color-base-300) / <alpha-value>)',
+      'base-content': 'rgb(var(--color-base-content) / <alpha-value>)',
+      primary: 'rgb(var(--color-primary) / <alpha-value>)',
+      border: 'rgba(var(--color-border))',
+    },
     extend: {
       fontFamily: {
+        sans: ['Rubik', ...fontFamily.sans],
+        mono: ['IBM Plex Mono', ...fontFamily.mono],
         ...fontFamily,
-        sans: [
-          'Rubik',
-          'ui-sans-serif',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          'Segoe UI',
-          'Roboto',
-          'Helvetica Neue',
-          'Arial',
-          'Noto Sans',
-          'sans-serif',
-          'Apple Color Emoji',
-          'Segoe UI Emoji',
-          'Segoe UI Symbol',
-          'Noto Color Emoji',
-        ],
-        mono: [
-          'IBM Plex Mono',
-          'ui-monospace',
-          'SFMono-Regular',
-          'Menlo',
-          'Monaco',
-          'Consolas',
-          'Liberation Mono',
-          'Courier New',
-          'monospace',
-        ],
       },
     },
-  },
-  plugins: [
-    require('daisyui'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
-  ],
-  daisyui: {
-    themes: [
-      {
-        dark: {
-          primary: '#24C8DB',
-          secondary: '#2F2F2F',
-          accent: '#FFC131',
-          neutral: '#f6f6f6',
-          'base-100': '#141414',
-          'base-200': '#1F1F1F',
-          'base-300': '#292929',
-          info: '#24C8DB',
-          success: '#23db69',
-          warning: '#FFC131',
-          error: '#db2c23',
-        },
-      },
-    ],
   },
 }
