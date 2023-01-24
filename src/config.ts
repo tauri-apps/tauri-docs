9c5abbb1ecf3534c94c58408fe213e08e7961bd9
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-01-24 10:21:22 +0000

    Refactor navbar menu

---
+++ src/config.ts
@@ -0,0 +1,42 @@
+type navbarItem = {
+  title: string
+  href: string
+  children?: navbarItem[]
+}
+
+const navbar: navbarItem[] = [
+  {
+    title: 'Docs',
+    href: '/docs',
+  },
+  {
+    title: 'References',
+    href: '/api',
+    children: [
+      {
+        title: 'JavaScript API',
+        href: '/api/core-js',
+      },
+      {
+        title: 'Rust API',
+        href: '/api/core-rust',
+      },
+      {
+        title: 'Tauri Config',
+        href: '/api/core-config',
+      },
+      {
+        title: 'Tauri CLI',
+        href: '/api/core-cli',
+      },
+    ],
+  },
+  {
+    title: 'About',
+    href: '/about',
+  },
+]
+
+export default {
+  navbar: navbar,
+}
