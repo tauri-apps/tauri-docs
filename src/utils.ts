6923324c728c96f178e673d40d1bc97dca24e798
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-01-28 16:04:47 +0000

    Build out i18n utilities and routing

---
+++ src/utils.ts
@@ -0,0 +1,33 @@
+import { locales } from './config'
+
+// Build paths for all locales. Should be used together with `getStaticPaths()`
+export const buildLocalizedStaticPaths = (params?: object, props?: object) => [
+  { params: { locale: undefined, ...params }, props: { ...props } },
+  ...locales
+    .filter((locale) => locale !== 'en')
+    .map((locale) => ({
+      params: { locale: locale, ...params },
+      props: { ...props },
+    })),
+]
+
+// Updates the slug of localized documents from this: `[locale]/[...slug]`
+// ...to this: `[locale]/[basePath]/[...slug]
+// Will also remove `en` from slugs as English is treated as the default route
+export const transformRelativeLocalizedEntrySlug = (
+  slug: string,
+  basePath?: string
+) => {
+  const [locale, ...rest] = slug.split('/')
+  const path = []
+
+  if (basePath !== undefined) {
+    path.push(basePath)
+  }
+
+  if (locale !== 'en') {
+    path.push(locale)
+  }
+
+  return path.concat(rest).join('/')
+}
