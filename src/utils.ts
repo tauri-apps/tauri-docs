24a3e50b9f34d6c1a6ad4d3b2ba1516a5f51d00e
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-01-29 22:01:52 +0000

    Optimize i18n building

--- src/utils.ts
+++ src/utils.ts
@@ -2,39 +2,47 @@ import { astroI18n } from 'astro-i18n'
 import { getCollection, getEntryBySlug } from 'astro:content'
 import type { collections } from './content/config'
 
-type CollectionKeys = keyof typeof collections
-
-export async function getDefaultLangCollection(c: CollectionKeys) {
-  // TODO: Fix types
-  const defaultLangCollection = await getCollection(c, ({ slug }) =>
-    slug.startsWith(astroI18n.defaultLangCode)
+export async function geti18nCollection<C extends keyof typeof collections>(
+  collection: C,
+  lang: LangCode
+) {
+  // Get entries from a collection for the default lang
+  const defaultLangCollection = await getCollection(collection, ({ slug }) =>
+    slug.startsWith(`${astroI18n.defaultLangCode}/`)
   )
 
-  // For each default lang entry, build a route for each lang
-  return defaultLangCollection.flatMap((entry) => {
-    const supportedLangEntries = astroI18n.supportedLangCodes.map(
-      async (lang) => {
-        // Builds out a dynamic slug per lang
-        const slug = `${lang}/${entry.slug.slice(
-          astroI18n.defaultLangCode.length + 1
-        )}`
-        // Falls back to the default lang entry if it doesn't exist
-        return (await getEntryBySlug(c, slug)) || entry
-      }
-    )
+  // Iterate through each of the entries for the default lang
+  const allEntries = await Promise.all(
+    defaultLangCollection.map(async (entry) => {
+      const baseSlug = entry.slug.slice(astroI18n.defaultLangCode.length + 1)
 
-    let defaultLangEntry = entry
-
-    // Fixes the slug if default lang is at the root
-    if (!astroI18n.showDefaultLangCode) {
-      const defaultLangEntrySlug = defaultLangEntry.slug.slice(
-        astroI18n.defaultLangCode.length + 1
-      )
-      defaultLangEntry = {
-        ...entry,
-        slug: defaultLangEntrySlug,
+      // If we're getting for the default lang then return the entry
+      if (lang === astroI18n.defaultLangCode) {
+        if (!astroI18n.showDefaultLangCode) {
+          return {
+            ...entry,
+            slug: baseSlug,
+          }
+        } else {
+          return entry
+        }
       }
-    }
-    return [defaultLangEntry, ...supportedLangEntries]
-  })
+
+      // Attempt to load the localized entry
+      const localizedSlug = `${lang}/${baseSlug}`
+      const localizedEntry = await getEntryBySlug(collection, localizedSlug)
+
+      // Fall back to the default lang entry
+      if (!localizedEntry) {
+        return {
+          ...entry,
+          slug: baseSlug,
+        }
+      }
+
+      return localizedEntry
+    })
+  ).then((result) => result.flat())
+
+  return allEntries
 }
