f1ae43cb9fae469bd2c05b9c6886d56b7a706dde
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-01-29 19:13:54 +0000

    Optimise getDefaultLangCollection

--- src/utils.ts
+++ src/utils.ts
@@ -5,21 +5,31 @@ import type { collections } from './content/config'
 type CollectionKeys = keyof typeof collections
 
 export async function getDefaultLangCollection(c: CollectionKeys) {
-  const collection = await getCollection(c, ({ slug }) => {
-    return slug.startsWith(astroI18n.defaultLangCode)
-  })
-  return collection.map((entry) => ({
-    ...entry,
-    slug: entry.slug.slice(astroI18n.defaultLangCode.length + 1),
-  }))
-}
-
-// This takes the defaultLangCode slug and transforms it into the respective entry
-export async function getLocalizedEntry(c: CollectionKeys, entry: any) {
-  const localizedEntry = await getEntryBySlug(
-    c,
-    `${astroI18n.langCode}/${entry.slug}`
+  // Get all the entries for the default lang code
+  const collection = await getCollection(c, ({ slug }) =>
+    slug.startsWith(astroI18n.defaultLangCode)
   )
 
-  return localizedEntry
+  // For each default lang entry, build a route for each lang
+  return collection.flatMap((entry) => {
+    const supportedLangEntries = astroI18n.supportedLangCodes.map(
+      async (lang) => {
+        // Builds out a dynamic slug per lang
+        const slug = `${lang}/${entry.slug.slice(
+          astroI18n.defaultLangCode.length + 1
+        )}`
+        // Falls back to the default lang entry if it doesn't exist
+        return (await getEntryBySlug(c, slug)) || entry
+      }
+    )
+    let defaultEntry = entry
+
+    if (!astroI18n.showDefaultLangCode) {
+      defaultEntry = {
+        ...entry,
+        slug: defaultEntry.slug.slice(astroI18n.defaultLangCode.length + 1),
+      }
+    }
+    return [defaultEntry, ...supportedLangEntries]
+  })
 }
