653a53e26e66a6429af7be50b80e0e0ac8bfd861
amrbashir <amr.bashir2015@gmail.com>
2023-02-03 17:40:06 +0200

    feat: switch to `meilisearch-docsearch`

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -1,26 +1,19 @@
 import React, { useEffect } from 'react'
 import './style.css'
+import 'meilisearch-docsearch/css'
 
 export default function Component() {
   useEffect(() => {
-    const DocsSearchBar = require('docs-searchbar.js').default
+    const docsearch = require('meilisearch-docsearch').default
 
-    DocsSearchBar({
-      hostUrl: 'https://ms-4ebb96f179f0-1619.fra.meilisearch.io',
+    docsearch({
+      host: 'https://ms-4ebb96f179f0-1619.fra.meilisearch.io',
       apiKey:
         '3eb6db150af1abefe000f00386e149dfb5a006932cab55d1ccd810b8672a4e12',
       indexUid: 'docs-v1',
-      inputSelector: '#search-bar-input',
-      debug: process.env.NODE_ENV === 'development',
+      container: '#docsearch',
     })
   }, [])
 
-  return (
-    <input
-      type="search"
-      id="search-bar-input"
-      className="navbar__search navbar__search-input"
-      placeholder="Search"
-    />
-  )
+  return <div id="docsearch" />
 }
