173ba80c2735fd9e126f69330e0be81b455a3fc2
amrbashir <amr.bashir2015@gmail.com>
2023-02-07 17:38:30 +0200

    update to 0.4

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -5,13 +5,15 @@ import 'meilisearch-docsearch/css'
 export default function Component() {
   useEffect(() => {
     const docsearch = require('meilisearch-docsearch').default
-    docsearch({
+    const destroy = docsearch({
       host: 'https://ms-4ebb96f179f0-1619.fra.meilisearch.io',
       apiKey:
         '3eb6db150af1abefe000f00386e149dfb5a006932cab55d1ccd810b8672a4e12',
       indexUid: 'docs-v1',
       container: '#docsearch',
     })
+
+    return () => destroy()
   }, [])
 
   return <div id="docsearch" />
