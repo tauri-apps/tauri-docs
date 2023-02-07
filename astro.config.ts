f2e08c7cf240eef4e68425367eb99a078dab9903
amrbashir <amr.bashir2015@gmail.com>
2023-02-07 21:52:16 +0200

    feat: add meilisearch-docsearch

--- astro.config.ts
+++ astro.config.ts
@@ -1,33 +1,36 @@
-import { defineConfig } from 'astro/config'
-import prefetch from '@astrojs/prefetch'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
-import i18n from 'astro-i18n'
-import Icons from 'unplugin-icons/vite'
-import { extractImageClass } from './src/plugins/remark-extract-image-class'
-import { nonEnglishLocales } from './astro.i18n.config'
+import { defineConfig } from 'astro/config';
+import prefetch from '@astrojs/prefetch';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import i18n from 'astro-i18n';
+import Icons from 'unplugin-icons/vite';
+import { extractImageClass } from './src/plugins/remark-extract-image-class';
+import { nonEnglishLocales } from './astro.i18n.config';
+
+// https://astro.build/config
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tauri-dev.netlify.app',
-  integrations: [
-    i18n(),
-    prefetch(),
-    tailwind(),
-    sitemap({
-      i18n: {
-        defaultLocale: 'en',
-        locales: {
-          en: 'en',
-          ...nonEnglishLocales.reduce((a, v) => ({ ...a, [v]: v }), {}),
-        },
-      },
-    }),
-  ],
+  integrations: [i18n(), prefetch(), tailwind(), sitemap({
+    i18n: {
+      defaultLocale: 'en',
+      locales: {
+        en: 'en',
+        ...nonEnglishLocales.reduce((a, v) => ({
+          ...a,
+          [v]: v
+        }), {})
+      }
+    }
+  }), solidJs()],
   vite: {
-    plugins: [Icons({ compiler: 'astro' })],
+    plugins: [Icons({
+      compiler: 'astro'
+    })]
   },
   markdown: {
-    remarkPlugins: [extractImageClass],
-  },
-})
+    remarkPlugins: [extractImageClass]
+  }
+});
