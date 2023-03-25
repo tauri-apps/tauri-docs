04ce685dbf6bff3e8db89e45a37fa02720f95fee
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-03-25 20:01:44 +0000

    Change metadata schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,8 @@
 import { z, defineCollection } from 'astro:content'
 
 const metadataSchema = {
-  meta_title: z.string().optional(),
-  meta_description: z.string().optional(),
+  title: z.string().optional(),
+  description: z.string().optional(),
   meta_position: z.number().optional(),
   i18n_ready: z.boolean().optional(),
 }
