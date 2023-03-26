79080f6bee8d9b1446eece7bbef4d92549a49809
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-03-26 17:01:49 +0100

    Fix local dev

--- astro.config.ts
+++ astro.config.ts
@@ -12,7 +12,7 @@ import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tauri-dev.netlify.app',
-  trailingSlash: 'always',
+  // trailingSlash: 'always',
   integrations: [
     i18n(),
     prefetch(),
