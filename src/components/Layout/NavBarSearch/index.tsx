73487b1ebb24491507f3c87d03eba7dc36b30fa1
FabianLars <fabianlars@fabianlars.de>
2023-05-06 18:45:59 +0200

    Add initial automatic search index update

--- src/components/Layout/NavBarSearch/index.tsx
+++ src/components/Layout/NavBarSearch/index.tsx
@@ -6,7 +6,7 @@ const Search: Component<{ lang: string }> = ({ lang }) => {
     <DocSearch
       host="https://ms-a1a1d899778e-1619.fra.meilisearch.io"
       apiKey="2cd51aecd5cbb326d9abaf5b95393e92009ac8a795bb928a21be0550449b797c"
-      indexUid="docs-dev"
+      indexUid="docs-v1"
       searchParams={{ filter: [`lang = ${lang}`] }}
     />
   )
