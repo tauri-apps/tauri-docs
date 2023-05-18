0b25295121283030b82e5b43315034111ffc02d8
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-05-18 18:24:01 +0100

    Remove text breaking

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -57,10 +57,6 @@ module.exports = {
             '--tw-prose-th-borders': theme('colors.base-content/25'),
             '--tw-prose-td-borders': theme('colors.base-content/25'),
             maxWidth: 'unset',
-            wordBreak: 'break-all',
-            table: {
-              wordBreak: 'normal',
-            },
           },
         },
       }),
