e2ce5533fa1c0c4ac317a4b038ee00660ee5f3a8
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-05-23 13:36:37 +0100

    Update api routing

--- src/utils.ts
+++ src/utils.ts
@@ -26,33 +26,68 @@ export const filterFragments = (collection: any[]) => {
   })
 }
 
-// Handle params
 export function createLocalizedPaths(callback?: (lang: any) => any) {
   return async () => {
-    return astroI18n.langCodes.flatMap((lang) => {
-      // Determine what the lang route should be
-      let langRoute: string | undefined = lang
-      if (
-        !astroI18n.showDefaultLangCode &&
-        lang === astroI18n.defaultLangCode
-      ) {
-        langRoute = undefined
-      }
-      // Build the base params
-      const template = {
-        params: {
-          lang: langRoute,
-        },
-      }
-      // Return just the lang route if no callback
-      if (!callback) {
-        return template
-      }
-      // Join all the objects together
-      return {
-        ...template,
-        ...callback(lang),
-      }
-    })
+    const mapped = await Promise.all(
+      astroI18n.langCodes.flatMap(async (lang) => {
+        // Determine what the lang route should be
+        let langRoute: string | undefined = lang
+        if (
+          !astroI18n.showDefaultLangCode &&
+          lang === astroI18n.defaultLangCode
+        ) {
+          langRoute = undefined
+        }
+
+        // Build the base params
+        const template = {
+          params: {
+            lang: langRoute,
+          },
+        }
+        // Return just the lang route if no callback
+        if (!callback) {
+          return template
+        }
+
+        const results = await callback(lang)
+
+        // Join all the objects together
+        return results.map((result: any) => {
+          return merge(result, template)
+        })
+      })
+    )
+
+    return mapped.flat()
   }
+}
+
+// Adapted from https://attacomsian.com/blog/javascript-merge-objects
+const merge = (...props: any) => {
+  // create a new object
+  let target = {}
+
+  // deep merge the object into the target object
+  const merger = (obj) => {
+    for (let prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        if (Object.prototype.toString.call(obj[prop]) === '[object Object]') {
+          // if the property is a nested object
+          target[prop] = merge(target[prop], obj[prop])
+        } else {
+          // for regular property
+          target[prop] = obj[prop]
+        }
+      }
+    }
+  }
+
+  // iterate through all objects and
+  // deep merge them with target
+  for (let i = 0; i < props.length; i++) {
+    merger(props[i])
+  }
+
+  return target
 }
