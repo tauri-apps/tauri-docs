75ff4216f805613b6ec63199cb4c148ff04407ea
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-05-23 12:32:51 +0100

    Build createLocalizedPaths utility function

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { astroI18n } from 'astro-i18n'
+
 export const sortCollection = (collection: any[]) => {
   collection.sort((a, b) => {
     // Sort on position
@@ -22,4 +24,35 @@ export const filterFragments = (collection: any[]) => {
   return collection.filter((entry) => {
     return !entry.slug.split('/').some((value: string) => value.startsWith('_'))
   })
+}
+
+// Handle params
+export function createLocalizedPaths(callback?: (lang: any) => any) {
+  return async () => {
+    return astroI18n.langCodes.flatMap((lang) => {
+      // Determine what the lang route should be
+      let langRoute: string | undefined = lang
+      if (
+        !astroI18n.showDefaultLangCode &&
+        lang === astroI18n.defaultLangCode
+      ) {
+        langRoute = undefined
+      }
+      // Build the base params
+      const template = {
+        params: {
+          lang: langRoute,
+        },
+      }
+      // Return just the lang route if no callback
+      if (!callback) {
+        return template
+      }
+      // Join all the objects together
+      return {
+        ...template,
+        ...callback(lang),
+      }
+    })
+  }
 }
