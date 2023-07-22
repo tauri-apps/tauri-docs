928ff5e55aa5373a2ffaa57d6242a7a1c7b1bfcb
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-07-22 18:01:08 +0100

    Initial customizations
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,41 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
-      social: {
-        github: 'https://github.com/withastro/starlight',
+      title: "Tauri",
+      logo: {
+        src: "./src/assets/logo.svg",
+        replacesTitle: true,
       },
+      social: {
+        github: "https://github.com/tauri-apps/tauri",
+        discord: "https://github.com/tauri-apps/tauri",
+        mastodon: "https://github.com/tauri-apps/tauri",
+        twitter: "https://github.com/tauri-apps/tauri",
+      },
+      editLink: {
+        baseUrl: "https://github.com/tauri-apps/tauri-docs/edit/dev",
+      },
+      customCss: ["./src/styles/custom.css"],
       sidebar: [
         {
-          label: 'Guides',
+          label: "Guides",
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
+            { label: "Example Guide", link: "/guides/example/" },
           ],
         },
         {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
+          label: "Reference",
+          autogenerate: { directory: "reference" },
         },
       ],
     }),
   ],
 
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+  image: { service: { entrypoint: "astro/assets/services/sharp" } },
 });
