ad5040fe2ee8e739e5a63e72be9b76edb997c142
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-07-24 20:52:47 +0100

    Shift some docs around even more
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,50 +22,75 @@ export default defineConfig({
       customCss: ["./src/styles/custom.css"],
       sidebar: [
         {
-          label: "Getting Started",
+          label: "Quick Start",
           items: [
-            { label: "What is Tauri?", link: "/v2/tutorials" },
+            { label: "Why Tauri?", link: "/v2/tutorials" },
             { label: "Prerequisites", link: "/v2/tutorials/prerequisites" },
             {
               label: "Create a Project",
               link: "/v2/tutorials/create-project/",
             },
             {
-              label: "Project Structure",
-              link: "/v2/tutorials/architecture/",
+              label: "Plugins",
+              link: "/v2/plugins/",
             },
           ],
         },
         {
           label: "Guides",
           items: [
-            // {
-            //   label: "Inter-Process Communication (IPC)",
-            //   link: "/guides/example/",
-            // },
-            { label: "Customize", link: "/v2/guides/customize/" },
             {
               label: "Build & Distribute",
+              link: "/v2/guides/build-distribute",
+            },
+            { label: "Customize", link: "/v2/guides/customize/" },
+            {
+              label: "Inter-Process Communication",
+              link: "#",
+            },
+            {
+              label: "Create a Plugin",
               link: "/v2/guides/build-distribute",
             },
             {
               label: "Troubleshoot",
               link: "/v2/guides/troubleshoot",
             },
-          ],
-        },
-        {
-          label: "Plugins",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "What are Plugins?", link: "/guides/example/" },
-            { label: "Plugin Directory", link: "/guides/example/" },
-            { label: "Develop a Plugin", link: "/guides/example/" },
+            {
+              label: "More Guides",
+              link: "/v2/guides/",
+            },
           ],
         },
         {
           label: "Reference",
-          autogenerate: { directory: "v2/reference" },
+          items: [
+            {
+              label: "Project Structure",
+              link: "/v2/reference/customize/",
+            },
+            {
+              label: "Command Line Interface (CLI)",
+              link: "/v2/reference/customize/",
+            },
+            {
+              label: "Tauri Config",
+              link: "/v2/guides/troubleshoot",
+            },
+            {
+              label: "JavaScript API",
+              link: "/v2/guides/build-distribute",
+            },
+            {
+              label: "Rust API",
+              link: "/v2/guides/build-distribute",
+            },
+            {
+              label: "Error Reference",
+              link: "/v2/guides/build-distribute",
+            },
+          ],
+          // autogenerate: { directory: "v2/reference" },
         },
         {
           label: "TODO",
