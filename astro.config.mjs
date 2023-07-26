e043aa53059ddcc5f7703fa41241d699385608a0
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-07-26 16:25:58 +0000

    Add experimental flag for assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -90,7 +90,7 @@ export default defineConfig({
         },
         {
           label: "TODO",
-          autogenerate: { directory: "2/todo", collapsed: true },
+          autogenerate: { directory: "2/todo", },
         },
       ],
     }),
@@ -100,7 +100,9 @@ export default defineConfig({
       langs: ["powershell"],
     },
   },
-
+  experimental: {
+    assets: true
+  },
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
   image: { service: { entrypoint: "astro/assets/services/sharp" } },
 });
