0c98972ba73cc5f043707b41466d1b8f21e67754
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-07-30 01:20:44 +0100

    add custom since tag
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>

--- packages/tauri-typedoc-theme/src/index.ts
+++ packages/tauri-typedoc-theme/src/index.ts
@@ -2,6 +2,7 @@ import type {
   Application,
   Comment,
   DeclarationReflection,
+  Options,
   PageEvent,
   Reflection,
   SignatureReflection,
@@ -12,6 +13,15 @@ import {
 } from 'typedoc-plugin-markdown';
 
 export class TauriThemeContext extends MarkdownThemeRenderContext {
+  #markdownThemeRenderContext: MarkdownThemeRenderContext;
+
+  constructor(event: PageEvent<Reflection> | null, options: Options) {
+    super(event, options);
+    this.#markdownThemeRenderContext = new MarkdownThemeRenderContext(
+      event,
+      options
+    );
+  }
   override sources: (
     reflection: DeclarationReflection | SignatureReflection,
     headingLevel: number
@@ -26,6 +36,39 @@ export class TauriThemeContext extends MarkdownThemeRenderContext {
     );
 
     return label + sources.join(', ');
+  };
+
+  override comment: (
+    comment: Comment,
+    headingLevel?: number | undefined
+  ) => string = (comment, headingLevel) => {
+    const filteredComment = { ...comment } as Comment;
+    filteredComment.blockTags = [];
+
+    const customTags = [];
+
+    for (const blogTag of comment.blockTags) {
+      if (blogTag.tag === '@since') {
+        customTags.push(blogTag);
+      } else {
+        filteredComment.blockTags.push(blogTag);
+      }
+    }
+
+    let markdown = this.#markdownThemeRenderContext.comment(
+      filteredComment,
+      headingLevel
+    );
+
+    for (const customCommentTag of customTags) {
+      if (customCommentTag.content.length > 0) {
+        markdown += `**Since**: ${customCommentTag.content
+          .map((content) => content.text)
+          .join(', ')}`;
+      }
+    }
+
+    return markdown;
   };
 }
 
