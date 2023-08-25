85b49ba23d30ce9ab65d5bde17a537450b7804bb
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-08-25 22:14:24 +0100

    revert
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,6 @@ import { i18nSchema } from '@astrojs/starlight/schema';
 import { docsAndBlogSchema } from 'starlight-blog/schema';
 
 export const collections = {
-  docs: defineCollection({ schema: docsAndBlogSchema }),
-  i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
+	docs: defineCollection({ schema: docsAndBlogSchema }),
+	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
