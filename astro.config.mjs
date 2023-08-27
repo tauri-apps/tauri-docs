720e7edc8c9ea49dc73e6bab11fc8ca24eda2015
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-08-27 15:19:34 +0100

    Blog (#1357)
    * add blog integration
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * initial blog post transfer
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * fix image
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * revert
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * remove unneeded image
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * add data file
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * fix broken links
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * fix more broken links
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * add a few redirects
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * finish redirects
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * add blog integration
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * initial blog post transfer
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * revert
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * fix merge conflicts
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * resolve git conflict
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * setup author config
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * formatting updates
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * add author title (upcoming upstream feature)
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    ---------
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,52 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import locales from './locales.json';
 import configGenerator from './src/plugins/configGenerator';
 import starlightLinksValidator from 'starlight-links-validator';
+import starlightBlog from 'starlight-blog';
 
 await configGenerator();
+
+const authors = {
+	nothingismagick: {
+		name: 'Daniel Thompson-Yvetot',
+		title: 'Tauri Co-Founder',
+		picture: 'https://github.com/nothingismagick.png',
+	},
+	lucasfernog: {
+		name: 'Lucas Nogueira',
+		title: 'Tauri Co-Founder',
+		picture: 'https://github.com/lucasfernog.png',
+	},
+	beanow: {
+		name: 'Robin van Boven',
+		title: 'Tauri Board Director',
+		picture: 'https://github.com/Beanow.png',
+	},
+	jbolda: {
+		name: 'Jacob Bolda',
+		title: 'Tauri Board Director',
+		picture: 'https://github.com/jbolda.png',
+	},
+	lorenzolewis: {
+		name: 'Lorenzo Lewis',
+		title: 'Tauri Community Lead',
+		picture: 'https://github.com/lorenzolewis.png',
+	},
+	tweidinger: {
+		name: 'Tillmann Weidinger',
+		title: 'Tauri Security',
+		picture: 'https://github.com/tweidinger.png',
+	},
+	amrbashir: {
+		name: 'Amr Bashir',
+		title: 'Tauri Development',
+		picture: 'https://github.com/amrbashir.png',
+	},
+	wusyong: {
+		name: 'Wu Yu Wei',
+		title: 'Tauri Development Lead',
+		picture: 'https://github.com/wusyong.png',
+	},
+};
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,6 +61,7 @@ export default defineConfig({
 	},
 	integrations: [
 		starlightLinksValidator(),
+		starlightBlog({ authors }),
 		starlight({
 			title: 'Tauri',
 			description: 'The cross-platform app building toolkit',
@@ -159,4 +204,24 @@ export default defineConfig({
 	},
 	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
 	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+	redirects: {
+		// Old blog url schema redirects
+		'/blog/2022-06-19-tauri-1-0': '/blog/tauri-1-0',
+		'/blog/2022-07-12-tauri-programme-turns-1-and-board-elections':
+			'/blog/tauri-programme-turns-1-and-board-elections',
+		'/blog/2022-09-15-tauri-1-1': '/blog/tauri-1-1',
+		'/blog/2022-09-19-tauri-egui-0-1': '/blog/tauri-egui-0-1',
+		'/blog/2022-11-18-tauri-1-2': '/blog/tauri-1-2',
+		'/blog/2022-12-09-tauri-mobile-alpha': '/blog/tauri-mobile-alpha',
+		'/blog/2023-02-03-tauri-2-0-0-alpha-3': '/blog/tauri-2-0-0-alpha-3',
+		'/blog/2023-02-09-tauri-community-growth-and-feedback':
+			'/blog/tauri-community-growth-and-feedback',
+		'/blog/2023-03-01-create-tauri-app-version-3-released':
+			'/blog/create-tauri-app-version-3-released',
+		'/blog/2023-03-20-tauri-2-0-0-alpha-4': '/blog/tauri-2-0-0-alpha-4',
+		'/blog/2023-05-03-tauri-1-3': '/blog/tauri-1-3',
+		'/blog/2023-06-14-tauri-1-4': '/blog/tauri-1-4',
+		'/blog/2023-06-15-tauri-board-elections-and-governance-updates':
+			'/blog/tauri-board-elections-and-governance-updates',
+	},
 });
