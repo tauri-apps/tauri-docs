35d5ac60cbec84d626fdbe91260c8fa24c32fd1c
Vitor <vasfvitor@gmail.com>
2023-10-20 06:29:40 -0300

    fix: shikiConfig (#1635)

--- astro.config.mjs
+++ astro.config.mjs
@@ -194,7 +194,7 @@ export default defineConfig({
 	],
 	markdown: {
 		shikiConfig: {
-			langs: ['powershell'],
+			langs: ['powershell', 'ts', 'rust', 'bash', 'json', 'toml'],
 		},
 		rehypePlugins: [
 			rehypeHeadingIds,
