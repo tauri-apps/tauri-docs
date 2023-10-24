a4dc0ff13f9da4ef59b71015518dc1cf138443bb
vasfvitor <gitkey@virtuaires.com.br>
2023-10-24 14:05:03 -0300

    add reference
    renamed const

--- src/pages/social-images/[...path].png.ts
+++ src/pages/social-images/[...path].png.ts
@@ -1,4 +1,4 @@
-process.env.FONTCONFIG_PATH = "/opt/build/repo/public/fonts/";
+process.env.FONTCONFIG_PATH = '/opt/build/repo/public/fonts/';
 
 // TODO: Define default text
 const og = {
@@ -14,7 +14,7 @@ import { createBlogTemplate, createDefaultTemplate } from '@assets/dynamic-og/te
 const cjs = createRequire(import.meta.url);
 const sharp = cjs('sharp');
 
-const blogPosts = Object.fromEntries(
+const docsContent = Object.fromEntries(
 	Object.entries(import.meta.glob('../../content/docs/**/*.{md,mdx}')).map(([path, getInfo]) => {
 		path = path.replaceAll('../', '');
 		path = path.replace('.mdx', '');
@@ -23,11 +23,11 @@ const blogPosts = Object.fromEntries(
 		return [path, getInfo];
 	})
 );
-
+// Reference: https://docs.astro.build/en/reference/api-reference/#getstaticpaths
 export async function getStaticPaths() {
 	const paths = ['index', 'blog', 'features/index'];
 
-	for (const [path, getInfo] of Object.entries(blogPosts)) {
+	for (const [path, getInfo] of Object.entries(docsContent)) {
 		const info = (await getInfo()) as Record<string, any>;
 
 		if (!info.frontmatter.draft) {
@@ -37,9 +37,9 @@ export async function getStaticPaths() {
 
 	return paths.map((path) => ({ params: { path } }));
 }
-
-export async function GET({ params, request }) {
-	const getInfo = blogPosts[params.path];
+// Reference: https://docs.astro.build/en/core-concepts/endpoints/#params-and-dynamic-routing
+export async function GET({ params }) {
+	const getInfo = docsContent[params.path];
 	let template;
 
 	if (getInfo) {
