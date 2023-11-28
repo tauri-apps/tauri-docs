fce9631d0a2b37e7de09ed26c8df09d2b943cbce
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-11-28 23:05:57 +0000

    Big chunk o' changes

--- packages/config-generator/build.ts
+++ packages/config-generator/build.ts
@@ -1,8 +1,22 @@
 import { JSONSchema7, JSONSchema7Definition } from 'json-schema';
 import { existsSync, writeFileSync } from 'node:fs';
 
-// TODO: Check AssetProtocolConfig > properties > scope, is it an array?
-// TODO: Add constraints directly after type
+/**
+ * TODO
+ * - Move simple defaults to be after type
+ * - Links for $ref
+ * - Nested properties (BeforeDevCommand, PatternKind)
+ * - renderSimpleTypes for `null` values (BuildConfig > [beforeDevCommand, beforeBundle, beforeBuildCommand], BundleConfig > [externalBin, fileAssociations, resources], DebConfig > depends, NsisConfig > languages)
+ * - Array type rendering for BuildConfig > features
+ * - list rendering in descriptions (main config)
+ * - Optional: Swap type and description for "simple" layouts such as anyOf (BundleTarget)
+ * - OneOf rendering (BundleType, NSISInstallerMode, PatternKind, Theme, TitleBarStyle, WebviewInstallMode, WindowEffect, WindowEffectState, WindowsUpdateInstallMode)
+ * - Enum rendering (WebviewInstallMode, WindowEffect, WindowEffectState, WindowsUpdateInstallMode)
+ * - additionalProperties (DebConfig > files)
+ * - AnyOf for nulls (SecurityConfig > [csp, devCsp], TauriConfig > trayIcon, WindowEffectsConfig > [color, state], WindowsConfig > [nsis, wix])
+ * - Maximum height of codeblocks
+ * - Cap markdown heading to 6
+ */
 
 const schemaFile = '../tauri/core/tauri-config-schema/schema.json';
 const outputFile = '../../src/content/docs/2/reference/config.md';
@@ -13,24 +27,25 @@ if (!existsSync(schemaFile)) {
 
 let schema: JSONSchema7 = await import(schemaFile);
 
-const out = ['---', 'title: Configuration', '---'];
+const output = ['---', 'title: Configuration', '---'];
 
-out.push(
+output.push(
 	...buildSchemaDefinition(schema, {
-		headingLevel: 1,
+		headingLevel: 2,
 		renderDefault: false,
 		renderTitle: false,
 		renderLineBreak: true,
 	})
 );
 
-writeFileSync(outputFile, out.join('\n\n'));
+writeFileSync(outputFile, output.join('\n\n'));
 
 interface Options {
 	headingLevel: number;
 	renderTitle: boolean;
 	renderDefault: boolean;
 	renderLineBreak: boolean;
+	renderSimpleTypes: boolean;
 }
 
 function buildSchemaDefinition(
@@ -44,6 +59,7 @@ function buildSchemaDefinition(
 			renderTitle: true,
 			renderDefault: true,
 			renderLineBreak: false,
+			renderSimpleTypes: false,
 		},
 		passedOptions
 	);
@@ -52,92 +68,91 @@ function buildSchemaDefinition(
 		return [`\`${schema}\``];
 	}
 
-	const unhandledKeys = ['$id'];
-	unhandledKeys.forEach((key) => {
-		// Check for unhandled keys
-		if (Object.keys(schema).includes(key)) {
-			throw Error(`Unhandled key: ${key}`);
-		}
-	});
+	// TODO:
+	const keys = [
+		// Meta data
+		'examples',
+
+		// Applicators
+		// any
+		'not',
+		'if',
+		'then',
+		'else',
+	];
 
 	const out = [];
+
+	/**
+	 * Metadata
+	 * Renders: title, readOnly, writeOnly, description, default, examples
+	 */
+	opts.renderTitle && schema.title && out.push(`${'#'.repeat(opts.headingLevel)} ${schema.title}`);
+
+	if (schema.readOnly || schema.writeOnly) {
+		const line = [];
+		schema.readOnly && line.push('Read only');
+		schema.writeOnly && line.push('Write only');
+		out.push(line.join(' & '));
+	}
+
+	schema.description &&
+		out.push(
+			// Sets headings to appropriate level
+			`${schema.description.replaceAll(/#{1,6}(?=.+[\n\\n])/g, '#'.repeat(opts.headingLevel))}`
+		);
+
+	/**
+	 * Type validation & annotations
+	 * Renders: type, enum, const
+	 */
+	let types = '';
+
+	if (schema.type) {
+		// TODO: If object then link to properties heading?
+		if (!opts.renderSimpleTypes) {
+			types += '**Type**: ';
+		}
+
+		if (Array.isArray(schema.type)) {
+			types += schema.type.map((value) => '`' + value + '`').join(' | ');
+		} else {
+			// TODO: If array then special handling
+			types += '`' + schema.type + '`';
+		}
+	}
+
+	if (schema.enum) {
+		if (!schema.enum.every((value) => typeof value === 'string')) {
+			throw Error(`Complex enum not handled: ${JSON.stringify(schema.enum)}`);
+		}
+
+		// TODO: Formatting
+		types += `Enum: ${schema.enum.map((value) => '`' + value + '`').join(' | ')}`;
+	}
+
+	if (schema.const) {
+		throw Error('Not implemented');
+	}
+
+	/**
+	 * String constraints: maxLength, minLength, pattern, format, contentMediaType, contentEncoding
+	 * Numeric constraints: multipleOf, maximum, exclusiveMaximum, minimum, exclusiveMinimum
+	 * Object constraints: maxProperties, minProperties
+	 * Array constraints: maxItems, minItems, uniqueItems
+	 */
 	const constraints = [];
 
-	// Schema Annotations
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-10
-	schema.title && opts.renderTitle && out.push(`${'#'.repeat(opts.headingLevel)} ${schema.title}`);
-	// FIXME: Doing this as a way to temporarily escape MD formatting
-	schema.description && out.push(`${schema.description}`);
+	// String
+	schema.maxLength && constraints.push(`maximum length of \`${schema.maxLength}\``);
+	schema.minLength && constraints.push(`minimum length of \`${schema.minLength}\``);
+	schema.pattern && constraints.push(`pattern of \`${schema.pattern}\``);
+	schema.format && constraints.push(`formatted as \`${schema.format}\``);
+	schema.contentMediaType &&
+		constraints.push(`content media type of \`${schema.contentMediaType}\``);
+	schema.contentEncoding && constraints.push(`content encoding of \`${schema.contentEncoding}\``);
 
-	// Validation Keywords for Any Instance Type
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-6.1
-	if (schema.type) {
-		let line = '**Type**: ';
-		if (typeof schema.type === 'string') {
-			line += '`' + schema.type + '`';
-		} else {
-			line += schema.type.map((value) => `\`${value}\``).join(' | ');
-		}
-		out.push(line);
-	}
-
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01
-	// TODO: Slug & Link
-	schema.$id && out.push(`**$id**: ${schema.$id}`);
-	if (schema.$ref) {
-		const last = schema.$ref.split('/').pop();
-		if (!last) {
-			throw Error(`Unknown ref: ${schema.$ref}`);
-		}
-		out.push(`\`${last}\``);
-	}
-
-	// Keywords for Applying Subschemas With Boolean Logic
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-6.7
-	if (schema.allOf) {
-		if (schema.allOf.length != 1) {
-			throw Error('Unhandled all of');
-		}
-		out.push(
-			`**Type**: ${buildSchemaDefinition(schema.allOf[0], {
-				headingLevel: opts.headingLevel + 1,
-			}).join('\n\n')}`
-		);
-	}
-	// TODO: Determine if there are multiple
-	// If so, list them out in a list or table
-	// Look at `BeforeDevCommand` and `AppUrl` for examples
-	if (schema.anyOf) {
-		out.push(
-			`**AnyOf**: ${schema.anyOf.map((value) =>
-				buildSchemaDefinition(value, {
-					headingLevel: opts.headingLevel + 1,
-				}).join('\n\n')
-			)}`
-		);
-	}
-	// TODO: If multiple then render in a list or table.
-	// `WindowEffect` as an example
-	if (schema.oneOf) {
-		out.push(
-			`**OneOf**: ${schema.oneOf.map((value) =>
-				buildSchemaDefinition(value, {
-					headingLevel: opts.headingLevel + 1,
-				}).join('\n\n')
-			)}`
-		);
-	}
-
-	// TODO:
-	schema.not && out.push(`**not**: ${schema.not}`);
-
-	// Schema Annotations
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-10
-	schema.readOnly && constraints.push(`is read only`);
-	schema.writeOnly && constraints.push(`is write only`);
-
-	// Validation Keywords for Numeric Instances (number and integer)
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-6.2
+	// Number
 	schema.multipleOf && constraints.push(`multiple of \`${schema.multipleOf}\``);
 	schema.maximum && constraints.push(`maximum of \`${schema.maximum}\``);
 	schema.exclusiveMaximum &&
@@ -146,102 +161,143 @@ function buildSchemaDefinition(
 	schema.exclusiveMinimum &&
 		constraints.push(`exclusive minimum of \`${schema.exclusiveMinimum}\``);
 
-	// Validation Keywords for Strings
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-6.3
-	schema.maxLength && constraints.push(`maximum length of \`${schema.maxLength}\``);
-	schema.minLength && constraints.push(`minimum length of \`${schema.minLength}\``);
-	schema.pattern && constraints.push(`pattern of \`${schema.pattern}\``);
+	// Object
+	schema.maxProperties && constraints.push(`maximum of \`${schema.maxProperties}\` properties`);
+	schema.minProperties && constraints.push(`minimum of \`${schema.minProperties}\` properties`);
 
-	// Validation Keywords for Arrays
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-6.4
-	schema.maxItems && constraints.push(`maximum items of \`${schema.maxItems}\``);
-	schema.minItems && constraints.push(`minimum items of \`${schema.minItems}\``);
-	schema.uniqueItems && constraints.push(`items must be unique: \`${schema.uniqueItems}\``);
-
-	// Validation Keywords for Objects
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-6.5
-	schema.maxProperties && constraints.push(`maximum properties: \`${schema.maxProperties}\``);
-	schema.minProperties && constraints.push(`minimum properties: \`${schema.minProperties}\``);
-
-	// Semantic Validation With "format"
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-7
-	schema.format && constraints.push(`formatted as \`${schema.format}\``);
-
-	// String-Encoding Non-JSON Data
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-8
-	schema.contentMediaType && constraints.push(`content media type: \`${schema.contentMediaType}\``);
-	schema.contentEncoding && constraints.push(`content encoding: \`${schema.contentEncoding}\``);
-
-	// Validation Keywords for Objects
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-6.5
-	if (schema.properties) {
-		out.push(`${'#'.repeat(opts.headingLevel + 1)} Properties`);
-		Object.entries(schema.properties)
-			.filter(([key]) => key !== '$schema')
-			.sort(([a], [b]) => a.localeCompare(b))
-			.forEach(([key, value]) => {
-				out.push(`${'#'.repeat(opts.headingLevel + 2)} ${key}`);
-				out.push(...buildSchemaDefinition(value, { headingLevel: opts.headingLevel + 2 }));
+	// Array
+	if (schema.items) {
+		const itemsDefinitions = [];
+		if (Array.isArray(schema.items)) {
+			schema.items.map((item) => {
+				const definition = buildSchemaDefinition(item, { ...opts, renderSimpleTypes: true });
+				if (definition.length == 1) {
+					itemsDefinitions.push(definition);
+				} else {
+					throw Error(`Non-simple item: ${item}`);
+				}
 			});
-	}
-
-	// MARK: TODO
-
-	// Schema Annotations
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-10
-	schema.examples && out.push(`**examples**: ${schema.examples}`);
-
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01
-	schema.$id && out.push(`**$id**: ${schema.$id}`);
-	// `schema.$schema` is purposefully ignored
-	schema.$comment && out.push(`**$comment**: ${schema.$comment}`);
-
-	// Validation Keywords for Any Instance Type
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-6.1
-	schema.enum && out.push(`**enum**: ${schema.enum}`);
-	schema.const && out.push(`**const**: ${schema.const}`);
-
-	// Validation Keywords for Arrays
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-6.4
-	schema.items && out.push(`**items**: ${JSON.stringify(schema.items)}`);
-	schema.additionalItems && out.push(`**additionalItems**: ${schema.additionalItems}`);
-	schema.contains && out.push(`**contains**: ${schema.contains}`);
-
-	// Validation Keywords for Objects
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-6.5
-	// TODO:
-	schema.required && out.push(`**required**: ${schema.required}`);
-	schema.patternProperties && out.push(`**patternProperties**: ${schema.patternProperties}`);
-	schema.additionalProperties &&
-		out.push(`**additionalProperties**: ${schema.additionalProperties}`);
-	schema.dependencies && out.push(`**dependencies**: ${schema.dependencies}`);
-	schema.propertyNames && out.push(`**propertyNames**: ${schema.propertyNames}`);
-
-	// Keywords for Applying Subschemas Conditionally
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-6.6
-	schema.if && out.push(`**if**: ${schema.if}`);
-	schema.then && out.push(`**then**: ${schema.then}`);
-	schema.else && out.push(`**else**: ${schema.else}`);
-
-	if (constraints.length != 0) {
-		out.push(`**Constraints**: ${constraints.join(', ')}`);
-	}
-
-	// Schema Annotations
-	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-10
-	if (schema.default && opts.renderDefault) {
-		if (typeof schema.default !== 'object') {
-			// Simple default
-			out.push(`**Default Value**: \`${schema.default}\``);
-		} else if (Object.keys(schema.default).length == 0) {
-			// Empty object
-			out.push(`**Default Value**: \`${JSON.stringify(schema.default)}\``);
 		} else {
-			// Object with properties
+			const definition = buildSchemaDefinition(schema.items, { ...opts, renderSimpleTypes: true });
+			if (definition.length == 1) {
+				itemsDefinitions.push(definition);
+			} else {
+				throw Error(`Non-simple item: ${schema.items}`);
+			}
+		}
+		types += `<${itemsDefinitions.map((value) => value).join(' | ')}>`;
+	}
+	schema.maxItems && constraints.push(`maximum of \`${schema.maxItems}\` items`);
+	schema.minItems && constraints.push(`minimum of \`${schema.minItems}\` items`);
+	schema.uniqueItems && constraints.push(`each item must be unique`);
+
+	if (constraints.length > 0) {
+		if (opts.renderSimpleTypes) {
+			types += ' (' + constraints.join(', ') + ')';
+		} else {
+			types += ', ' + constraints.join(', ') + '.';
+		}
+	}
+
+	if (types.length > 0) {
+		out.push(types);
+	}
+
+	if (schema.contains) {
+		throw Error('Not implemented');
+	}
+	if (schema.additionalItems) {
+		throw Error('Not implemented');
+	}
+
+	// Object properties
+	// Renders: properties, required
+	// TODO: object
+	// 'patternProperties',
+	// 'additionalProperties',
+	// 'dependencies',
+	// 'propertyNames',
+	if (schema.properties) {
+		out.push(`${'#'.repeat(opts.headingLevel)} Properties`);
+		const sortedProperties = Object.entries(schema.properties)
+			.filter(([key]) => key !== '$schema')
+			.sort(([a], [b]) => a.localeCompare(b));
+		out.push(
+			sortedProperties
+				.map(([key]) => `- \`${key}\`${schema.required?.includes(key) ? ' (required)' : ''}`)
+				.join('\n')
+		);
+		// TODO: List out the properties with an anchor to each one
+		sortedProperties.forEach(([key, value]) => {
+			out.push(`${'#'.repeat(opts.headingLevel + 1)} ${key}`);
 			out.push(
-				`\`\`\`json title="Default Value"\n${JSON.stringify(schema.default, null, '\t')}\n\`\`\`\n`
+				buildSchemaDefinition(value, { ...opts, headingLevel: opts.headingLevel + 2 }).join('\n\n')
+			);
+		});
+	}
+
+	// TODO: Need a way to show a simplified view of this
+	if (schema.allOf) {
+		if (schema.allOf.length > 1) {
+			throw Error('All of with more than one schema found');
+		}
+		const definition = buildSchemaDefinition(schema.allOf[0]);
+		if (definition.length == 1) {
+			out.push(`${opts.renderSimpleTypes ? '' : '**Type**: '}${definition}`);
+		} else {
+			throw Error(`Non-simple definition for allOf: ${JSON.stringify(definition)}`);
+		}
+	}
+
+	if (schema.anyOf) {
+		out.push(`**Any of the following types**:`);
+		out.push(
+			schema.anyOf
+				.map((value) => {
+					// TODO: Formatting
+					return `- ${buildSchemaDefinition(value, opts).join('  \n')}`;
+				})
+				.join('\n')
+		);
+	}
+
+	if (schema.oneOf) {
+		out.push(`**One of**:`);
+		Object.entries(schema.oneOf).forEach(([key, value]) => {
+			out.push(`\`${key}\``);
+			out.push(...buildSchemaDefinition(value, opts));
+		});
+	}
+
+	// TODO: Might move this below the type, could potentially combine with type if a simple type (not an object)
+	if (opts.renderDefault && schema.default) {
+		if (typeof schema.default !== 'object') {
+			// Simple default value
+			out.push(`**Default**: \`${schema.default}\``);
+		} else if (Object.keys(schema.default).length == 0) {
+			// Empty object, render on a single line
+			out.push(`**Default**: \`${JSON.stringify(schema.default)}\``);
+		} else {
+			// Object with properties, render in code block
+			out.push(
+				`\n\`\`\`json title='Default'\n${JSON.stringify(schema.default, null, '\t')}\n\`\`\`\n`
 			);
 		}
+	}
+
+	if (schema.examples) {
+		throw Error('Examples not implemented');
+	}
+
+	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01
+	// TODO: Slug & Link
+	// schema.$id && out.push(`**$id**: ${schema.$id}`);
+	if (schema.$ref) {
+		const last = schema.$ref.split('/').pop();
+		if (!last) {
+			throw Error(`Unknown ref: ${schema.$ref}`);
+		}
+		out.push(`\`${last}\``);
 	}
 
 	// Schema Re-Use With "$defs"
@@ -249,13 +305,14 @@ function buildSchemaDefinition(
 	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-9
 	const definitions = { ...schema.$defs, ...schema.definitions };
 	if (Object.keys(definitions).length > 0) {
-		out.push(`${'#'.repeat(opts.headingLevel + 1)} Definitions`);
+		out.push(`${'#'.repeat(opts.headingLevel)} Definitions`);
 		Object.entries(definitions)
 			.sort(([a], [b]) => a.localeCompare(b))
 			.forEach(([key, value]) => {
-				out.push(`${'#'.repeat(opts.headingLevel + 2)} ${key}`);
+				out.push(`${'#'.repeat(opts.headingLevel + 1)} ${key}`);
 				out.push(
 					...buildSchemaDefinition(value, {
+						...opts,
 						headingLevel: opts.headingLevel + 2,
 						renderLineBreak: true,
 					})
@@ -263,7 +320,7 @@ function buildSchemaDefinition(
 			});
 	}
 
-	opts.renderLineBreak && out.push('---');
+	// opts.renderLineBreak && out.push('---');
 
 	return out;
 }
