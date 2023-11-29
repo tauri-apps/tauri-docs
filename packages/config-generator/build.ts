764155ac25eae8eb769109ea608eae0e50941b8f
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-11-29 23:09:24 +0000

    commits

--- packages/config-generator/build.ts
+++ packages/config-generator/build.ts
@@ -1,14 +1,6 @@
 import { JSONSchema7, JSONSchema7Definition, JSONSchema7TypeName } from 'json-schema';
 import { existsSync, writeFileSync } from 'node:fs';
-
-/**
- * TODO
- * - Maximum height for default codeblocks
- * - Links for $ref objects
- * - Labels for types
- * - Array null values (BeforeDevCommand > cwd)
- * - Sub-definition generation (BeforeDevCommand)
- */
+import { slug } from 'github-slugger';
 
 const schemaFile = '../tauri/core/tauri-config-schema/schema.json';
 const outputFile = '../../src/content/docs/2/reference/config.md';
@@ -17,14 +9,13 @@ if (!existsSync(schemaFile)) {
 	throw Error('Could not find the Tauri config schema. Is the Tauri submodule initialized?');
 }
 
-let schema: JSONSchema7 = await import(schemaFile);
+let schema: JSONSchema7 = (await import(schemaFile)).default;
 
 const output = ['---\ntitle: Configuration\n---'];
 
 output.push(
 	...buildSchemaDefinition(schema, {
 		headingLevel: 2,
-		renderDefault: false,
 		renderTitle: false,
 	})
 );
@@ -34,21 +25,17 @@ writeFileSync(outputFile, output.join('\n\n'));
 interface Options {
 	headingLevel: number;
 	renderTitle: boolean;
-	renderDefault: boolean;
-	renderStyle: 'list' | 'section';
 }
 
 function buildSchemaDefinition(
 	schema: JSONSchema7Definition,
 	passedOptions: Partial<Options> = {}
 ): string[] {
-	// Assign options with default values
+	// Assign default values for missing options
 	const opts = Object.assign(
 		{
 			headingLevel: 1,
 			renderTitle: true,
-			renderDefault: true,
-			renderStyle: 'section',
 		},
 		passedOptions
 	);
@@ -70,73 +57,64 @@ function buildSchemaDefinition(
 		out.push(line.join(' & '));
 	}
 
-	if (opts.renderStyle === 'section') {
-		schema.description && out.push(buildDescription(schema.description, opts));
-	}
+	schema.description && out.push(buildDescription(schema.description, opts));
 
 	/**
 	 * Summary of type information
 	 */
-	const typeSummary: string[] = [];
+	const typeInfo: string[] = [];
 
 	if (schema.$ref) {
-		// TODO: Link & formatting
 		const ref = schema.$ref.split('/').pop();
 		if (!ref) {
 			throw Error(`Invalid ref: ${schema.$ref}`);
 		}
-		typeSummary.push('`' + ref + '`');
+		typeInfo.push(`[\`${ref}\`](#${slug(ref)})`);
 	}
 
-	// TODO: Formatting
 	if (schema.type) {
 		if (Array.isArray(schema.type)) {
-			typeSummary.push(schema.type.map((value) => buildType(value, schema, opts)).join(' | '));
+			typeInfo.push(schema.type.map((value) => buildType(value, schema, opts)).join(' | '));
 		} else {
-			const typeInfo = buildType(schema.type, schema, opts);
-			typeInfo && typeSummary.push(typeInfo);
+			const info = buildType(schema.type, schema, opts);
+			info && typeInfo.push(info);
 		}
 	}
 
 	if (schema.enum) {
-		typeSummary.push(schema.enum.map((value) => `\`"${value}"\``).join(' | '));
+		typeInfo.push(schema.enum.map((value) => `\`"${value}"\``).join(' | '));
 	}
 
-	let buildSubDefinitions = false;
+	// Need a way to indicate if this should be built out into a more complicated type
 	if (schema.allOf) {
 		const { output, multipleDefinitions, needSubDefinition } = buildXOf(schema.allOf, opts);
 		if (multipleDefinitions) {
-			out.push('**All of the following types**:');
-			out.push(output.map((value) => `- ${value.join(', ')}`).join('\n'));
+			typeInfo.push('**All of the following types**:');
+			typeInfo.push(output.map((value) => `- ${value.join(', ')}`).join('\n'));
 		} else {
-			out.push(...output.flat());
+			typeInfo.push(...output.flat());
 		}
-		buildSubDefinitions = needSubDefinition;
 	}
 
 	if (schema.anyOf) {
 		const { output, multipleDefinitions, needSubDefinition } = buildXOf(schema.anyOf, opts);
 		if (multipleDefinitions) {
-			out.push('**Any of the following types**:');
-			out.push(output.map((value) => `- ${value.join(', ')}`).join('\n'));
+			typeInfo.push('**Any of the following types**:');
+			typeInfo.push(output.map((value) => `- ${value.join(', ')}`).join('\n'));
 		} else {
-			out.push(...output.flat());
+			typeInfo.push(...output.flat());
 		}
-		buildSubDefinitions = needSubDefinition;
 	}
 
 	if (schema.oneOf) {
 		const { output, multipleDefinitions, needSubDefinition } = buildXOf(schema.oneOf, opts);
 		if (multipleDefinitions) {
-			out.push('**One of the following types**:');
-			out.push(output.map((value) => `- ${value.join(', ')}`).join('\n'));
+			typeInfo.push('**One of the following types**:');
+			typeInfo.push(output.map((value) => `- ${value.join(', ')}`).join('\n'));
 		} else {
-			out.push(...output.flat());
+			typeInfo.push(...output.flat());
 		}
-		buildSubDefinitions = needSubDefinition;
 	}
-
-	// const // what if it's complex?
 
 	/**
 	 * Constraints
@@ -168,27 +146,20 @@ function buildSchemaDefinition(
 	schema.uniqueItems && constraints.push(`each item must be unique`);
 
 	if (constraints.length > 0) {
-		typeSummary.push(constraints.join(', '));
+		typeInfo.push(constraints.join(', '));
 	}
 
-	// TODO: Formatting for if it should show label or not
-	if (typeSummary.length > 0) {
-		out.push(typeSummary.join(', '));
-	}
-
-	if (opts.renderStyle === 'list') {
-		schema.description && out.push(buildDescription(schema.description, opts));
+	if (typeInfo.length > 0) {
+		out.push(typeInfo.join(', '));
 	}
 
 	// MARK: Render Sub-Definitions
-	if (buildSubDefinitions) {
-		out.push(`${'#'.repeat(Math.min(6, opts.headingLevel))} Sub-Definitions`);
-		out.push('NEED TO BUILD SUBDEFINITION');
-	}
+	// if (buildSubDefinitions) {
+	// 	out.push(`${'#'.repeat(Math.min(6, opts.headingLevel))} Sub-Definitions`);
+	// 	out.push('NEED TO BUILD SUBDEFINITION');
+	// }
 
-	// TODO: The default on the root object should be nicer
-	// CSS for code blocks to limit height
-	if (opts.renderDefault && schema.default) {
+	if (schema.default) {
 		if (typeof schema.default !== 'object') {
 			// Render on single line
 			out.push(`**Default**: \`${schema.default}\``);
@@ -207,8 +178,7 @@ function buildSchemaDefinition(
 		throw Error('Examples not implemented');
 	}
 
-	if (schema.properties && opts.renderStyle !== 'list') {
-		// TODO: Check and throw if properties and trying to render in list view
+	if (schema.properties) {
 		out.push(`${'#'.repeat(Math.min(6, opts.headingLevel))} Properties`);
 		// Build list
 		const properties = Object.entries(schema.properties)
@@ -235,51 +205,9 @@ function buildSchemaDefinition(
 		);
 	}
 
-	/**
-	 * Renders the content in the following order
-	 * ✅ title
-	 * ✅ readOnly || writeOnly
-	 *
-	 * ✅ $ref
-	 * ✅ type
-	 * items
-	 * ✅ enum
-	 * const
-	 * if ([allof, anyOf, oneOf].count == 1) {
-	 * 	render
-	 * }
-	 * ✅ validations
-	 *
-	 * ✅ description
-	 * ✅ default
-	 * ✅ examples
-	 *
-	 * if ([allof, anyOf, oneOf].count != 1) {
-	 * 	render
-	 * }
-	 *
-	 * not
-	 *
-	 * ~Array~
-	 * additionalItems
-	 * contains
-	 *
-	 * ~Object~
-	 * properties (✅ required inline)
-	 * patternProperties
-	 * additionalProperties
-	 * dependencies
-	 * propertyNames
-	 *
-	 * ~Conditional~
-	 * if, then, else
-	 *
-	 * definitions & $defs
-	 */
-
-	// // Schema Re-Use With "$defs"
-	// // https://datatracker.ietf.org/doc/html/draft-bhutton-json-schema-00#section-8.2.4
-	// // https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-9
+	// Schema Re-Use With "$defs"
+	// https://datatracker.ietf.org/doc/html/draft-bhutton-json-schema-00#section-8.2.4
+	// https://datatracker.ietf.org/doc/html/draft-handrews-json-schema-validation-01#section-9
 	const definitions = { ...schema.$defs, ...schema.definitions };
 	if (Object.keys(definitions).length > 0) {
 		out.push(`${'#'.repeat(Math.min(opts.headingLevel, 6))} Definitions`);
@@ -326,7 +254,7 @@ function buildType(
 			items = [parentSchema.items];
 		}
 
-		// TODO: Formatting style
+		// TODO: Formatting style (AppUrl)
 		typeLine += items.map((item) => buildSchemaDefinition(item).join(', ')).join(' , ');
 	}
 
@@ -336,10 +264,6 @@ function buildType(
 
 	if (typeName !== 'array') {
 		typeLine += '`' + typeName + '`';
-	}
-	if (typeName === 'object' && opts.renderStyle === 'list') {
-		// TODO: Link
-		typeLine += ' (see sub-schema below)';
 	}
 	// TODO: Render const
 	return typeLine;
@@ -358,7 +282,6 @@ function buildXOf(
 
 		const definition = buildSchemaDefinition(value, {
 			...opts,
-			renderStyle: schema.length === 1 ? 'section' : 'list',
 			headingLevel: Math.min(6, opts.headingLevel + 1),
 		});
 
@@ -378,9 +301,6 @@ function buildXOf(
 }
 
 function buildDescription(description: string, opts: Options): string {
-	if (opts.renderStyle === 'list') {
-		description = description.replaceAll('\n\n', ' ');
-	}
 	// Sets headings to appropriate level
 	return `${description.replaceAll(
 		/#{1,6}(?=.+[\n\\n])/g,
