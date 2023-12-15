247986da53b60c8d36898fa0480ef555605498d2
vasfvitor <gitkey@virtuaires.com.br>
2023-12-15 15:49:04 -0300

    add comments on 00 hack

--- src/pages/0/0.ts
+++ src/pages/0/0.ts
@@ -1,5 +1,8 @@
 import { allPages } from '../../utils/content';
 
+// https://github.com/withastro/docs/blob/main/src/pages/0/0.ts
+// https://www.github.com/withastro/docs/pull/4266/commits/030073f32d6dfe586c6e1da8d48d6b5485541ba2
+
 export function GET() {
 	allPages[0];
 	return new Response('');
