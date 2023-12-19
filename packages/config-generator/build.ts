e99c9221c5dc23a11ec3e67716b5785bde989e42
lorenzolewis <lorenzolewis@users.noreply.github.com>
2023-12-19 14:06:57 +0000

    [ci] format

--- packages/config-generator/build.ts
+++ packages/config-generator/build.ts
@@ -11,7 +11,9 @@ if (!existsSync(schemaFile)) {
 
 let schema: JSONSchema7 = (await import(schemaFile)).default;
 
-const output = ['---\n# NOTE: This file is auto-generated in packages/config-generator/build.ts\n# For corrections please edit https://github.com/tauri-apps/tauri/blob/dev/core/tauri-utils/src/config.rs directly\n\ntitle: Configuration\n---'];
+const output = [
+	'---\n# NOTE: This file is auto-generated in packages/config-generator/build.ts\n# For corrections please edit https://github.com/tauri-apps/tauri/blob/dev/core/tauri-utils/src/config.rs directly\n\ntitle: Configuration\n---',
+];
 
 output.push(
 	...buildSchemaDefinition(schema, {
