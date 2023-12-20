20fd2e6382c99f68f12dd1b992c66d967e958bd4
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-12-20 17:15:10 +0000

    Migrate References (#1758)
    * add recipe drafts
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Add concepts
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Add IPC page
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Revert "Add concepts"
    
    This reverts commit e5f04279d73664705e51a86ce928bedaf66c000b.
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * fix broken link
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Revert "fix broken link"
    
    This reverts commit d025b6117a84e3fde1636fe50ae7b9d4f2011925.
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Add diagram SVGs
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Revert "Add diagram SVGs"
    
    This reverts commit c502f222354c0ccb4d33578e3727195fb4b0fd90.
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * add webview versions
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Add configuration files concept
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Add reference index
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Update sidebar
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Update concept TOC
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Move configuration files to references
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Add redirects
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Update references sidebar
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * update architecture links
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Update link format
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Revert "Update link format"
    
    This reverts commit 480e98edce9ced5f3402ff00e27b2babaf7b1a65.
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * Update redirects
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    * fix broken link
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>
    
    ---------
    
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -162,6 +162,10 @@ export default defineConfig({
 					label: 'References',
 					items: [
 						{
+							label: 'List of References',
+							link: '/references',
+						},
+						{
 							label: 'Tauri Configuration',
 							link: '2/reference/config',
 						},
@@ -238,6 +242,26 @@ export default defineConfig({
 			'/v1/guides/development/updating-dependencies',
 			'/guides/develop/updating-dependencies'
 		),
+		// v1 /references
+		...i18nRedirect('/v1/references', '/concepts'),
+		...i18nRedirect('/v1/references/architecture', '/concepts/architecture'),
+		...i18nRedirect('/v1/references/architecture/process-model', '/concepts/process-model'),
+		...i18nRedirect('/v1/references/architecture/security', '/concepts/tauri-security'),
+		...i18nRedirect(
+			'/v1/references/architecture/inter-process-communication',
+			'/concepts/inter-process-communication'
+		),
+		...i18nRedirect(
+			'/v1/references/architecture/inter-process-communication/brownfield',
+			'/concepts/inter-process-communication/brownfield'
+		),
+		...i18nRedirect(
+			'/v1/references/architecture/inter-process-communication/isolation',
+			'/concepts/inter-process-communication/isolation'
+		),
+		...i18nRedirect('/v1/references/security', '/concepts/development-security'),
+		...i18nRedirect('/v1/references/configuration-files', '/references/configuration-files'),
+		...i18nRedirect('/v1/references/webview-versions', '/references/webview-versions'),
 		// Decommissioned locales
 		'/ko/[...slug]': '/[...slug]',
 		'/it/[...slug]': '/[...slug]',
