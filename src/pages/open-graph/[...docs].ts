5f518df00cb6d3428aeb5da2afe9854b40f52cb8
vasfvitor <gitkey@virtuaires.com.br>
2023-12-20 01:02:38 -0300

    improve font size strategy and add comments

--- src/pages/open-graph/[...docs].ts
+++ src/pages/open-graph/[...docs].ts
@@ -6,14 +6,21 @@ import { allPages } from 'src/utils/content';
 const rtlLanguages = new Set(['']);
 const getLangFromSlug = (slug: CollectionEntry<'docs'>['slug']) => slug.split('/')[0];
 
-// TODO: Setup the line below to skip on non production builds
-// const paths = process.env.SKIP_OG ? [] : allPages;
+// TODO: Setup the process.env to skip on non production builds
 /** Paths for all of our Markdown content we want to generate OG images for. */
-const paths = allPages;
+const paths = process.env.SKIP_OG ? [] : allPages;
 
 /** An object mapping file paths to file metadata. */
 const pages = Object.fromEntries(paths.map(({ id, slug, data }) => [id, { data, slug }]));
 
+function getDate(date: Date): string {
+  // en-GB -> dd MM yy
+  return date.toLocaleDateString('en-GB', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+}
 
 /**
  * TODO: This can be improved
@@ -23,18 +30,23 @@ const pages = Object.fromEntries(paths.map(({ id, slug, data }) => [id, { data, 
  * @returns A string with "..." at the end if text is longer than set
  */
 function clamp(txt: string, fontSize: number, singleLine = false,): string {
-  // those numbers are what more or less fit to description and title size
+  // those numbers are what more or less fit to description and title size, not precisely
+  // it can vary based on font. Maybe this can help? https://github.com/adambisek/string-pixel-width/blob/master/src/pixelWidthCalculator.html
   let LEN = 54;
-  if (fontSize > 50) {
-    LEN = 24
+  let EXTRA = 16;
+  // this should be less than titleSize
+  if (fontSize > 60) {
+    LEN = 32
+    EXTRA = 10;
   }
-  let MAX_LEN = singleLine ? LEN : LEN + 10;
+  let MAX_LEN = singleLine ? LEN : LEN + EXTRA;
   if (txt.length > MAX_LEN) {
     txt = txt.trim().substring(0, MAX_LEN);
     txt[txt.length - 1] === '.' ? (txt += '..') : (txt += '...');
   }
   return txt;
 }
+
 
 // REFERENCE:
 // https://github.com/delucis/astro-og-canvas/tree/latest/packages/astro-og-canvas
@@ -43,40 +55,40 @@ export const { getStaticPaths, GET } = OGImageRoute({
   pages,
 
   // TODO: Limit title and description max-width then break into 2 lines, maybe need Intl.Segmenter and get current locale to do it properly
-
+  // TODO: Improve texts size and placement
   getImageOptions: async (_, { data, slug }: (typeof pages)[string]) => {
-    /** Those titleSize and descSize are coupled with @function clamp() */
-    const [titleSize, descSize, dateSize] = [72, 36, 27];
+    /** titleSize and descSize are coupled with @function clamp() */
+    let [titleSize, descSize, dateSize] = [96, 48, 32];
     let description = "";
     let postDate = "";
-    const title = clamp(data.title, titleSize);
-    if (data.description) { description = clamp(data.description, descSize); }
+    let title = clamp(data.title, titleSize);
+    if (data.description) {
+      titleSize *= 0.85; // 81,6
+      description = clamp(data.description, descSize);
+    }
     if (slug.startsWith('blog/') && data.date && data.excerpt) {
+      titleSize *= 0.75; // 72
+      descSize *= 0.75; // 36
       description = clamp(data.excerpt, descSize, true);
-      const date: Date = new Date(data.date);
-      // en-GB -> dd MM yy
-      postDate = date.toLocaleDateString('en-GB', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric',
-      });
+      postDate = getDate(new Date(data.date));
     }
 
     return {
       title,
       description,
+      // extraField was patched with sole purpose to place blog post date
       extraField: postDate,
       dir: rtlLanguages.has(getLangFromSlug(slug)) ? 'rtl' : 'ltr',
       padding: 66,
       bgImage: { path: './src/assets/og-bg.png' },
       logo: { path: './src/assets/og-logo.png' },
       font: {
-        title: {  
-           /** Size is coupled with @function clamp() */
+        title: {
+          /** Size is coupled with @function clamp() */
           size: titleSize,
           lineHeight: 1.25,
           weight: 'Bold',
-          families: ["Noto Sans Condensed", "Noto Sans SC"]
+          families: ["Noto Sans", "Noto Sans SC"]
         },
         description: {
           /** Size is coupled with @function clamp() */
@@ -86,7 +98,6 @@ export const { getStaticPaths, GET } = OGImageRoute({
           families: ["Noto Sans", "Noto Sans SC"]
         },
         extraField: {
-
           size: dateSize,
           lineHeight: 1.25,
           weight: 'Light',
@@ -94,7 +105,7 @@ export const { getStaticPaths, GET } = OGImageRoute({
         },
       },
       fonts: [
-        './src/pages/open-graph/_fonts/NotoSans_Condensed-Bold.ttf',
+        './src/pages/open-graph/_fonts/NotoSans-Bold.ttf',
         './src/pages/open-graph/_fonts/NotoSans-Regular.ttf',
         './src/pages/open-graph/_fonts/NotoSans-Light.ttf',
 
