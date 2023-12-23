cd7fbf0dc9a1f0860c6cfbd6314761c773cc202c
vasfvitor <gitkey@virtuaires.com.br>
2023-12-23 01:25:04 -0300

    font Inter, 2 lines, trim

--- src/pages/open-graph/[...docs].ts
+++ src/pages/open-graph/[...docs].ts
@@ -1,10 +1,9 @@
+import type { CollectionEntry } from 'astro:content';
 import { OGImageRoute } from 'astro-og-canvas';
 import { allPages } from 'src/utils/content';
 
-// TODO: 
-//import { rtlLanguages } from '~/i18n/languages';
-//import { getLangFromSlug } from '~/util';
-
+const rtlLanguages = new Set(['']);
+const getLangFromSlug = (slug: CollectionEntry<'docs'>['slug']) => slug.split('/')[0];
 
 // TODO: Setup the process.env to skip on non production builds
 /** Paths for all of our Markdown content we want to generate OG images for. */
@@ -25,85 +24,66 @@ function getDate(date: Date): string {
 /**
  * TODO: This can be improved
  * Helper function to clamp a string
- * @param txt Text to process
- * @param singleLine Limit into a single line or not (default to false)
- * @returns A string with "..." at the end if text is longer than set
+ * @returns A string with "..." at the end if text is longer than MAX_LEN
  */
-function clamp(txt: string, fontSize: number, singleLine = false,): string {
+function clampTwoLines(txt: string, fontSize: number): string {
   // those numbers are what more or less fit to description and title size, not precisely
-  // it can vary based on font. Maybe this can help? https://github.com/adambisek/string-pixel-width/blob/master/src/pixelWidthCalculator.html
-  let LEN = 54;
-  let EXTRA = 16;
-  // this should be less than titleSize
+  // it can vary based on font, as of now it matches Inter. 
+  // Maybe this can help? https://github.com/adambisek/string-pixel-width/blob/master/src/pixelWidthCalculator.html
+  let MAX_LEN = 73;
+  // title:
   if (fontSize > 60) {
-    LEN = 32
-    EXTRA = 10;
+    MAX_LEN = 48;
   }
-  let MAX_LEN = singleLine ? LEN : LEN + EXTRA;
   if (txt.length > MAX_LEN) {
-    txt = txt.trim().substring(0, MAX_LEN);
+    txt = txt.trim().substring(0, MAX_LEN).trim();
     txt[txt.length - 1] === '.' ? (txt += '..') : (txt += '...');
   }
   return txt;
 }
-
 
 // REFERENCE:
 // https://github.com/delucis/astro-og-canvas/tree/latest/packages/astro-og-canvas
 export const { getStaticPaths, GET } = OGImageRoute({
   param: 'docs',
   pages,
-
-  // TODO: Limit title and description max-width then break into 2 lines, maybe need Intl.Segmenter and get current locale to do it properly
-  // TODO: Improve texts size and placement
   getImageOptions: async (_, { data, slug }: (typeof pages)[string]) => {
-    /** titleSize and descSize are coupled with @function clamp() */
+    /** titleSize and descSize are coupled with @function clampTwoLines() */
     let [titleSize, descSize] = [72, 48];
     let description = "";
-    let title = clamp(data.title, titleSize);
+    let title = clampTwoLines(data.title, titleSize);
     if (data.description) {
-      //titleSize *= 0.85; // 81,6
-      description = clamp(data.description, descSize);
+      description = clampTwoLines(data.description, descSize);
     }
     if (slug.startsWith('blog/') && data.date && data.excerpt) {
-      //titleSize *= 0.75; // 72
-      //descSize *= 0.75; // 36
-      description = data.excerpt
-
+      description = clampTwoLines(data.excerpt, descSize);
     }
-
     return {
       title,
       description,
-      padding: 80,
+      dir: rtlLanguages.has(getLangFromSlug(slug)) ? 'rtl' : 'ltr',
+      padding: 66,
       bgImage: { path: './src/assets/og-bg.png' },
       logo: { path: './src/assets/og-logo.png' },
       font: {
         title: {
-          /** Size is coupled with @function clamp() */
+          /** Size is coupled with @function clampTwoLines() */
           size: titleSize,
           lineHeight: 1.25,
           weight: 'Normal',
-          families: ["Noto Sans", "Noto Sans SC"]
+          families: ["Inter", "Noto Sans SC", "Noto Sans"]
         },
         description: {
           /** Size is coupled with @function clamp() */
           size: descSize,
           lineHeight: 1.25,
           weight: 'Normal',
-          families: ["Noto Sans", "Noto Sans SC"]
+          families: ["Inter", "Noto Sans SC Variable"]
         }
       },
       fonts: [
-        './src/pages/open-graph/_fonts/NotoSans-Bold.ttf',
-        './src/pages/open-graph/_fonts/NotoSans-Regular.ttf',
-        './src/pages/open-graph/_fonts/NotoSans-Light.ttf',
-
-        // simplified chinese
-        './src/pages/open-graph/_fonts/NotoSansSC-Regular.ttf',
-        './src/pages/open-graph/_fonts/NotoSansSC-Bold.ttf',
-        './src/pages/open-graph/_fonts/NotoSansSC-Light.ttf',
-      ],
+        './node_modules/@fontsource-variable/inter/files/inter-latin-standard-normal.woff2',
+      ]
     };
   },
 });
