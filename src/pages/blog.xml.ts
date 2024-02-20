ec15542623ce0b4e25e3d6d9a4305bfd1361b5f0
Vitor Ayres <gitkey@virtuaires.com.br>
2024-02-20 17:47:35 -0300

    Update blog.xml.ts

--- src/pages/blog.xml.ts
+++ src/pages/blog.xml.ts
@@ -3,6 +3,7 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import type { APIContext } from 'astro';
 
+// https://docs.astro.build/en/reference/api-reference/#endpoint-context
 export async function GET(context: APIContext) {
 	const posts = await getCollection('docs', ({ id }) => {
 		return id.startsWith('blog');
@@ -10,16 +11,12 @@ export async function GET(context: APIContext) {
 	return rss({
 		title: 'Tauri Blog',
 		description: 'The cross-platform app building toolkit',
-		// https://docs.astro.build/en/reference/api-reference/#contextsite
 		// @ts-expect-error
 		site: context.site,
 		items: posts.map((post) => ({
 			pubDate: post.data.date,
 			description: post.data.excerpt,
 			...post.data,
-			// Generate a `url` from each post `slug`
-			// This assumes all blog posts are rendered as `/blog/[slug]` routes
-			// https://docs.astro.build/en/guides/content-collections/#generating-pages-from-content-collections
 			link: `/${post.slug}/`,
 		})),
 	});
