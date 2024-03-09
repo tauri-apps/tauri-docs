1cc52e50d30c12ebf1e9d8f4d941709808e5647c
vasfvitor <gitkey@virtuaires.com.br>
2024-03-09 01:11:40 -0300

    WIP refactor
    show link to GitHub
    wip version side navigation
    improve wording and navigation
    move releases to references

--- packages/releases-generator/build.ts
+++ packages/releases-generator/build.ts
@@ -7,34 +7,45 @@ const packages = [
 	{
 		name: 'tauri',
 		url: 'https://raw.githubusercontent.com/tauri-apps/tauri/dev/core/tauri/CHANGELOG.md',
+		tag: 'https://github.com/tauri-apps/tauri/releases/tag',
 	},
 	{
 		name: '@tauri-apps/api',
 		url: 'https://raw.githubusercontent.com/tauri-apps/tauri/dev/tooling/api/CHANGELOG.md',
+		tag: 'https://github.com/tauri-apps/tauri/releases/tag',
 	},
 	{
 		name: 'tauri-cli',
 		url: 'https://raw.githubusercontent.com/tauri-apps/tauri/dev/tooling/cli/CHANGELOG.md',
+		tag: 'https://github.com/tauri-apps/tauri/releases/tag',
 	},
 	{
 		name: '@tauri-apps/cli',
 		url: 'https://raw.githubusercontent.com/tauri-apps/tauri/dev/tooling/cli/node/CHANGELOG.md',
+		tag: 'https://github.com/tauri-apps/tauri/releases/tag',
 	},
 	{
 		name: 'tauri-bundler',
 		url: 'https://raw.githubusercontent.com/tauri-apps/tauri/dev/tooling/bundler/CHANGELOG.md',
+		tag: 'https://github.com/tauri-apps/tauri/releases/tag',
 	},
 	{
 		name: 'wry',
 		url: 'https://raw.githubusercontent.com/tauri-apps/wry/dev/CHANGELOG.md',
+		tag: 'https://github.com/tauri-apps/wry/releases/tag',
 	},
 	{
 		name: 'tao',
 		url: 'https://raw.githubusercontent.com/tauri-apps/tao/dev/CHANGELOG.md',
+		tag: 'https://github.com/tauri-apps/tao/releases/tag',
 	},
 ];
 
 const baseDir = '../../src/content/docs/releases';
+
+let latestVersions: {
+	[key: string]: string;
+} = {};
 
 async function generator() {
 	for (const pkg of packages) {
@@ -56,38 +67,23 @@ async function generator() {
 		mkdirSync(join(baseDir, pkg.name), { recursive: true });
 		//
 		/*
-		 * Write index file for each package
-		 */
-		const releaseFrontmatter = [
-			'---',
-			note,
-			`title: '${pkg.name}'`,
-			`slug: 'releases/${pkg.name}'`,
-			'prev: false',
-			'next: false',
-			`editUrl: 'https://github.com/tauri-apps/tauri-docs/packages/releases-generator/build.ts'`,
-			'---',
-		].join('\n');
-		const indexLink = `#### [All release](/releases)`;
-		writeFileSync(
-			join(baseDir, pkg.name, 'index.md'),
-			`${releaseFrontmatter}\n${indexLink}\n${releases.map((r) => `- [${r.version}](/releases/${pkg.name}/v${r.version})`).join('\n')}`
-		);
-
-		/*
 		 * Write files for each version
 		 */
 		let nextPage = '/releases';
 		let nextLabel = pkg.name;
 		const len = releases.length;
 		for (let i = 0; i < len; i++) {
+			/**
+			 * Deal with next-prev labels
+			 */
 			const thisVersion = releases[i].version;
 			let prevLabel, prevPage;
+			let navFrontmatter;
 			if (i !== len - 1) {
 				prevLabel = releases[i + 1].version;
 				prevPage = `releases/${pkg.name}/v${releases[i + 1].version}`;
 			}
-			let navFrontmatter;
+
 			if (i === len - 1) {
 				navFrontmatter = [
 					`prev: false`,
@@ -96,10 +92,11 @@ async function generator() {
 					`   label: '${nextLabel}'`,
 				];
 			} else if (i === 0) {
+				latestVersions[pkg.name] = `v${thisVersion}`;
 				navFrontmatter = [
 					`prev:`,
 					`   link: '${prevPage}'`,
-					`   label: '${prevPage}'`,
+					`   label: '${prevLabel}'`,
 					`next: false`,
 				];
 			} else {
@@ -113,24 +110,58 @@ async function generator() {
 				];
 			}
 
+			//
 			const pageFrontmatter = [
 				note,
 				`title: '${pkg.name}@${thisVersion}'`,
+				`description: '${thisVersion}'`,
 				`slug: 'releases/${pkg.name}/v${thisVersion}'`,
 				'template: splash',
 				`editUrl: 'https://github.com/tauri-apps/tauri-docs/packages/releases-generator/build.ts'`,
 			];
 
 			const frontmatter = ['---', ...pageFrontmatter, ...navFrontmatter, '---'].join('\n');
-			const indexLink = `[Return to: ${pkg.name}](/releases/${pkg.name})`;
+			//
+			const indexLink = `[Return](/releases)`;
+			const viewInGitHub = `<a href="${pkg.tag}/${pkg.name}-v${thisVersion}">View on GitHub</a>`;
+			const linksDiv = `<div style="margin-bottom:3rem; display: flex; justify-content: space-between; align-items: center"><span>${indexLink}</span><span>${viewInGitHub}</span></div>`;
+			//
+			const sidebar = `\nimport ReleaseSidebar from '@components/list/ReleaseSidebar.astro';
+			\n\n<ReleaseSidebar slug="releases/${pkg.name}"  packageName="${pkg.name}" />\n`;
+
 			writeFileSync(
-				join(baseDir, pkg.name, `v${thisVersion}.md`),
-				`${frontmatter}\n${indexLink}\n\n${releases[i].notes}`
+				join(baseDir, pkg.name, `v${thisVersion}.mdx`),
+				`${frontmatter}\n${sidebar}\n${linksDiv}\n${releases[i].notes}`
 			);
 			nextPage = `releases/${pkg.name}/v${thisVersion}`;
 			nextLabel = `v${thisVersion}`;
 		}
 	}
+
+	// Generate index page
+	const extraNote =
+		'# You can edit this file to quickly preview, but make sure you copy the changes over the source\n';
+	const indexPage = [
+		'---',
+		note,
+		extraNote,
+		`title: 'Tauri Core Ecosystem Releases'`,
+		`editUrl: 'https://github.com/tauri-apps/tauri-docs/packages/releases-generator/build.ts'`,
+		'---',
+	].join('\n');
+
+	const indexPageContent = `import { LinkCard, CardGrid } from '@astrojs/starlight/components';\n
+	<CardGrid>
+		<LinkCard title="tauri" href="/releases/tauri/${latestVersions['tauri']}" />
+		<LinkCard title="@tauri-apps/api" href="/releases/@tauri-apps/api/${latestVersions['@tauri-apps/api']}" />
+		<LinkCard title="tauri-cli (JavaScript)" href="/releases/tauri-cli/${latestVersions['tauri-cli']}" />
+		<LinkCard title="@tauri-apps/cli" href="/releases/@tauri-apps/cli/${latestVersions['@tauri-apps/cli']}" />
+		<LinkCard title="tauri-bundler" href="/releases/tauri-bundler/${latestVersions['tauri-bundler']}" />
+		<LinkCard title="wry" href="/releases/wry/${latestVersions['wry']}" />
+		<LinkCard title="tao" href="/releases/tao/${latestVersions['tao']}" />
+	</CardGrid>`;
+
+	writeFileSync(join(baseDir, 'index.mdx'), `${indexPage}\n${indexPageContent}`);
 }
 
 generator();
