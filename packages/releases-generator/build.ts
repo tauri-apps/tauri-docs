26553ce65f3b88ed56999d58a20df2b2d672f88e
vasfvitor <gitkey@virtuaires.com.br>
2024-03-09 02:03:53 -0300

    agora vai

--- packages/releases-generator/build.ts
+++ packages/releases-generator/build.ts
@@ -165,22 +165,24 @@ async function generator() {
 }
 
 function entitify(str: string): string {
-	return str.replace(/[&<>"']/g, function (entity) {
-		switch (entity) {
-			case '&':
-				return '&amp;';
-			case '<':
-				return '&lt;';
-			case '>':
-				return '&gt;';
-			case '"':
-				return '&quot;';
-			case "'":
-				return '&#39;';
-			default:
-				return entity;
-		}
-	});
+	return str
+		.replace(/[&<>"']/g, function (entity) {
+			switch (entity) {
+				case '&':
+					return '&amp;';
+				case '<':
+					return '&lt;';
+				case '>':
+					return '&gt;';
+				case '"':
+					return '&quot;';
+				case "'":
+					return '&#39;';
+				default:
+					return entity;
+			}
+		})
+		.replace(/\${/g, '$\{');
 }
 
 generator();
