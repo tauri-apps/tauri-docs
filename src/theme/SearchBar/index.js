64f9990763c5109118ba4647a38d58fc1ffd1493
FabianLars <fabianlars@fabianlars.de>
2024-05-22 13:02:54 +0200

    feat: Enable meilisearch analytics

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -8,7 +8,7 @@ export default function Component() {
 
     const docsearch = require('meilisearch-docsearch').default
     const destroy = docsearch({
-      host: 'https://ms-4ebb96f179f0-1619.fra.meilisearch.io',
+      host: 'https://edge.meilisearch.com',
       apiKey:
         '3eb6db150af1abefe000f00386e149dfb5a006932cab55d1ccd810b8672a4e12',
       indexUid: 'docs-v1',
