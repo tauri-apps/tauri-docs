ebe2ef9256e8608239343f35a107165b396cbe2c
Victor Aremu <me@victorare.mu>
2024-08-23 20:10:08 +0100

    refactor: move docsSchema to schemas dir

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,10 @@
-import { defineCollection, z } from 'astro:content';
-import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
-import { blogSchema } from 'starlight-blog/schema';
-import { tableOfContentsSchema } from 'src/schemas/tableOfContentsSchema';
+import { defineCollection } from 'astro:content';
+import { i18nSchema } from '@astrojs/starlight/schema';
+import { docsSchema } from 'src/schemas/docsSchema';
 
 export const collections = {
   docs: defineCollection({
-    schema: docsSchema({
-      extend: (context) =>
-        z.intersection(
-          blogSchema(context),
-          z.object({
-            tableOfContents: tableOfContentsSchema.optional(),
-          })
-        ),
-    }),
+    schema: docsSchema,
   }),
   i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
