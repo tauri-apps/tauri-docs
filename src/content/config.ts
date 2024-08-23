22f083551324392a767f2e42399835f48d8caa7c
Victor Aremu <me@victorare.mu>
2024-08-23 20:01:29 +0100

    refactor: move to schema dir

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,23 +1,7 @@
 import { defineCollection, z } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 import { blogSchema } from 'starlight-blog/schema';
-import { TableOfContentsSchema as BaseTableOfContentsSchema } from 'node_modules/@astrojs/starlight/schemas/tableOfContents';
-
-const tableOfContentsDefaults = {
-  collapseLevel: 1,
-  minHeadingLevel: 2,
-  maxHeadingLevel: 3,
-};
-
-const tableOfContentsSchema = z.intersection(
-  BaseTableOfContentsSchema(),
-  z.union([
-    z.object({
-      collapseLevel: z.number().min(0).default(1).optional(),
-    }),
-    z.boolean().transform((enabled) => (enabled ? tableOfContentsDefaults : false)),
-  ])
-);
+import { tableOfContentsSchema } from 'src/schemas/tableOfContentsSchema';
 
 export const collections = {
   docs: defineCollection({
