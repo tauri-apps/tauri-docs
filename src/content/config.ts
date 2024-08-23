57dc8c9e4a3f17bb1b7b9e1fbfc1c15409763058
Victor Aremu <me@victorare.mu>
2024-08-23 16:44:12 +0100

    feat: add collapse level to the table of contents schema
    of the docs schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,35 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 import { blogSchema } from 'starlight-blog/schema';
+import { TableOfContentsSchema as BaseTableOfContentsSchema } from 'node_modules/@astrojs/starlight/schemas/tableOfContents';
+
+const tableOfContentsDefaults = {
+  collapseLevel: 1,
+  minHeadingLevel: 2,
+  maxHeadingLevel: 3,
+};
+
+const tableOfContentsSchema = z.intersection(
+  BaseTableOfContentsSchema(),
+  z.union([
+    z.object({
+      collapseLevel: z.number().min(0).default(1).optional(),
+    }),
+    z.boolean().transform((enabled) => (enabled ? tableOfContentsDefaults : false)),
+  ])
+);
 
 export const collections = {
-  docs: defineCollection({ schema: docsSchema({ extend: (context) => blogSchema(context) }) }),
+  docs: defineCollection({
+    schema: docsSchema({
+      extend: (context) =>
+        z.intersection(
+          blogSchema(context),
+          z.object({
+            tableOfContents: tableOfContentsSchema.optional(),
+          })
+        ),
+    }),
+  }),
   i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
