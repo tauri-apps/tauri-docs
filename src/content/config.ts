d6f86b85c1e613900db5618635447307220b44fa
Ayres Vitor <gitkey@virtuaires.com.br>
2025-05-02 00:20:39 -0300

    temporarily update schema without toc specs

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,16 @@
 import { defineCollection } from 'astro:content';
-import { i18nSchema } from '@astrojs/starlight/schema';
-import { docsSchema } from 'src/schemas/docsSchema';
+import { i18nSchema, docsSchema } from '@astrojs/starlight/schema';
+// TODO fix docsSchema usage
+// import { docsSchema } from 'src/schemas/docsSchema';
+import { docsLoader, i18nLoader } from '@astrojs/starlight/loaders';
+import { blogSchema } from 'starlight-blog/schema';
 
 export const collections = {
   docs: defineCollection({
-    schema: docsSchema,
+    loader: docsLoader(),
+    schema: docsSchema({
+      extend: (context) => blogSchema(context),
+    }),
   }),
-  i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
+  i18n: defineCollection({ loader: i18nLoader(), schema: i18nSchema() }),
 };
