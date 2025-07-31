02b3b02b87da1d30b4b201e6653268ad26cec689
Ayres Vitor <gitkey@virtuaires.com.br>
2025-07-31 17:13:13 -0300

    fix path

--- packages/fetch-sponsors/config.ts
+++ packages/fetch-sponsors/config.ts
@@ -1,6 +1,12 @@
-export const OPEN_COLLECTIVE_FILE = '../../src/data/openCollectiveData.json';
-export const GITHUB_SPONSORS_FILE = '../../src/data/githubSponsorsData.json';
-export const GITHUB_CONTRIBUTORS_FILE = '../../src/data/githubContributorsData.json';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export const OPEN_COLLECTIVE_FILE = path.resolve(__dirname, '../../src/data/openCollectiveData.json');
+export const GITHUB_SPONSORS_FILE = path.resolve(__dirname, '../../src/data/githubSponsorsData.json');
+export const GITHUB_CONTRIBUTORS_FILE = path.resolve(__dirname, '../../src/data/githubContributorsData.json');
+
 
 export const PLATINUM_THRESHOLD = 5_000;
 export const GOLD_THRESHOLD = 500;
