cc050f0f8d05f2ec737bbb616b74352831b175e6
Ayres Vitor <gitkey@virtuaires.com.br>
2025-07-31 20:14:03 -0300

    add partners

--- packages/fetch-sponsors/config.ts
+++ packages/fetch-sponsors/config.ts
@@ -3,10 +3,18 @@ import path from 'path';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export const OPEN_COLLECTIVE_FILE = path.resolve(__dirname, '../../src/data/openCollectiveData.json');
-export const GITHUB_SPONSORS_FILE = path.resolve(__dirname, '../../src/data/githubSponsorsData.json');
-export const GITHUB_CONTRIBUTORS_FILE = path.resolve(__dirname, '../../src/data/githubContributorsData.json');
-
+export const OPEN_COLLECTIVE_FILE = path.resolve(
+  __dirname,
+  '../../src/data/openCollectiveData.json'
+);
+export const GITHUB_SPONSORS_FILE = path.resolve(
+  __dirname,
+  '../../src/data/githubSponsorsData.json'
+);
+export const GITHUB_CONTRIBUTORS_FILE = path.resolve(
+  __dirname,
+  '../../src/data/githubContributorsData.json'
+);
 
 export const PLATINUM_THRESHOLD = 5_000;
 export const GOLD_THRESHOLD = 500;
