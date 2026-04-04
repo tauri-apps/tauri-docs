b158d7c7f0345de30c25de9500bd4ba6793400ea
Tony <legendmastertony@gmail.com>
2026-04-05 01:40:24 +0800

    Fix paths

--- packages/fetch-sponsors/config.ts
+++ packages/fetch-sponsors/config.ts
@@ -1,19 +1,14 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
+import { searchForWorkspaceRoot } from 'vite';
+import process from 'node:process';
+import path from 'node:path';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const workspaceRoot = searchForWorkspaceRoot(process.cwd());
 
-export const OPEN_COLLECTIVE_FILE = path.resolve(
-  __dirname,
-  '../../src/data/openCollectiveData.json'
-);
-export const GITHUB_SPONSORS_FILE = path.resolve(
-  __dirname,
-  '../../src/data/githubSponsorsData.json'
-);
+export const OPEN_COLLECTIVE_FILE = path.resolve(workspaceRoot, 'src/data/openCollectiveData.json');
+export const GITHUB_SPONSORS_FILE = path.resolve(workspaceRoot, 'src/data/githubSponsorsData.json');
 export const GITHUB_CONTRIBUTORS_FILE = path.resolve(
-  __dirname,
-  '../../src/data/githubContributorsData.json'
+  workspaceRoot,
+  'src/data/githubContributorsData.json'
 );
 
 export const PLATINUM_THRESHOLD = 5_000;
