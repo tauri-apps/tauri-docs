46515300bc2d09049db60eebaedf3b0a25912531
Tony <legendmastertony@gmail.com>
2026-04-05 12:50:47 +0800

    Add reason comments

--- packages/fetch-sponsors/config.ts
+++ packages/fetch-sponsors/config.ts
@@ -2,6 +2,9 @@ import { searchForWorkspaceRoot } from 'vite';
 import process from 'node:process';
 import path from 'node:path';
 
+// FIXME: This is used by both this package and the starlight build environment,
+// so we can't use `import ProjectContext from 'virtual:starlight/project-context'` here
+// (starlight seems to copy all the dependency files out so dynamic relative paths don't work)
 const workspaceRoot = searchForWorkspaceRoot(process.cwd());
 
 export const OPEN_COLLECTIVE_FILE = path.resolve(workspaceRoot, 'src/data/openCollectiveData.json');
