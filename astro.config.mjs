a8ec638757a423c65f923db255d29394b3006b2e
Tony <legendmastertony@gmail.com>
2026-06-23 18:02:02 +0800

    Tweak table of content sidebar on desktop

--- astro.config.mjs
+++ astro.config.mjs
@@ -393,6 +393,7 @@ export default defineConfig({
         Header: './src/components/overrides/Header.astro',
         Footer: 'src/components/overrides/Footer.astro',
         ThemeSelect: 'src/components/overrides/ThemeSelect.astro',
+        TwoColumnContent: 'src/components/overrides/TwoColumnContent.astro',
       },
       head: [
         {
