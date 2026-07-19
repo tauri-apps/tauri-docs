514d7bebcc5bf9817f321d9db8a8fb6888f5cc4c
Ayres Vitor <gitkey@virtuaires.com.br>
2026-07-18 21:20:26 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -84,7 +84,7 @@ const authors = {
 
 const site = 'https://v2.tauri.app';
 
-const { plugins: typeDocPlugins } = getTauriTypeDocPlugins();
+const { plugins: typeDocPlugins, sidebarItems: typeDocSidebarItems } = getTauriTypeDocPlugins();
 
 // https://astro.build/config
 export default defineConfig({
@@ -343,11 +343,7 @@ export default defineConfig({
                 {
                   label: 'JavaScript',
                   collapsed: true,
-                  items: [
-                    {
-                      autogenerate: { directory: 'reference/javascript', collapsed: true },
-                    },
-                  ],
+                  items: typeDocSidebarItems,
                 },
                 {
                   label: 'Rust (docs.rs)',
