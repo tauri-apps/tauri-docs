9a0d55156de727704f2a85a35c7082a6d711189f
Ayres Vitor <gitkey@virtuaires.com.br>
2026-08-12 19:22:19 -0300

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -382,7 +382,12 @@ export default defineConfig({
         starlightLlmsTxt(llmsTxtConfig),
         ...(process.env.CONTEXT === 'deploy-preview'
           ? []
-          : [lunaria({ configPath: './lunaria.config.json', route: '/contribute/translate-status' })]),
+          : [
+              lunaria({
+                configPath: './lunaria.config.json',
+                route: '/contribute/translate-status',
+              }),
+            ]),
       ],
       title: 'Tauri',
       description: 'The cross-platform app building toolkit',
