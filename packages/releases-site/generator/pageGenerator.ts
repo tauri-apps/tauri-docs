a9aa16a629cb0d28c04671c18fb705e26882c3b5
Tony <68118705+Legend-Master@users.noreply.github.com>
2026-08-14 13:16:57 +0800

    Tweek release link styles (#4058)

--- packages/releases-site/generator/pageGenerator.ts
+++ packages/releases-site/generator/pageGenerator.ts
@@ -49,18 +49,26 @@ function buildGitHubReleaseUrl(repo: Repository, pkg: RepoPackage, version: stri
   return `${repo.repoUrl}/releases/tag/${tagBase}-v${version}`;
 }
 
+const cratesIoIcon = `<svg viewBox="0 0 14 16" fill="currentColor"><path d="M11.2468 4.54809L7 2L2.77581 4.53452L7.02308 6.98668L11.2468 4.54809ZM12 6.42265L8 8.73205V13.4L12 11V6.42265ZM2 6.396V11L6 13.4V8.7054L2 6.396ZM7 0L14 4V12L7 16L0 12V4L7 0Z" /></svg>`;
+const npmIcon = `<svg viewBox="0 0 24 12" fill="currentColor"><path d="M4 4V8H6V5H7V8H8V4H4ZM9 4V9H11V8H13V4H9ZM12 5V7H11V5H12ZM14 4V8H16V5H17V8H18V5H19V8H20V4H14ZM3 3H21V9H12V10H8V9H3V3Z" /></svg>`;
+const githubIcon = `<svg viewBox="0 0 24 24" fill="currentColor"><path d="M12,2.2467A10.00042,10.00042,0,0,0,8.83752,21.73419c.5.08752.6875-.21247.6875-.475,0-.23749-.01251-1.025-.01251-1.86249C7,19.85919,6.35,18.78423,6.15,18.22173A3.636,3.636,0,0,0,5.125,16.8092c-.35-.1875-.85-.65-.01251-.66248A2.00117,2.00117,0,0,1,6.65,17.17169a2.13742,2.13742,0,0,0,2.91248.825A2.10376,2.10376,0,0,1,10.2,16.65923c-2.225-.25-4.55-1.11254-4.55-4.9375a3.89187,3.89187,0,0,1,1.025-2.6875,3.59373,3.59373,0,0,1,.1-2.65s.83747-.26251,2.75,1.025a9.42747,9.42747,0,0,1,5,0c1.91248-1.3,2.75-1.025,2.75-1.025a3.59323,3.59323,0,0,1,.1,2.65,3.869,3.869,0,0,1,1.025,2.6875c0,3.83747-2.33752,4.6875-4.5625,4.9375a2.36814,2.36814,0,0,1,.675,1.85c0,1.33752-.01251,2.41248-.01251,2.75,0,.26251.1875.575.6875.475A10.0053,10.0053,0,0,0,12,2.2467Z"></path></svg>`;
+
 function buildExternalLinks(config: PackageConfig | undefined): PageLink[] {
   if (!config) {
     return [];
   }
   const links: PageLink[] = [];
+  links.push({ label: 'GitHub', icon: githubIcon, href: config.repo.repoUrl });
   if (config.pkg.cratesPath) {
-    links.push({ label: 'crates.io', href: cratesWebUrl(config.pkg.cratesPath) });
+    links.push({
+      label: 'crates.io',
+      icon: cratesIoIcon,
+      href: cratesWebUrl(config.pkg.cratesPath),
+    });
   }
   if (config.pkg.npmPath) {
-    links.push({ label: 'npm', href: npmWebUrl(config.pkg.npmPath) });
+    links.push({ label: 'npm', icon: npmIcon, href: npmWebUrl(config.pkg.npmPath) });
   }
-  links.push({ label: 'GitHub', href: config.repo.repoUrl });
   return links;
 }
 
