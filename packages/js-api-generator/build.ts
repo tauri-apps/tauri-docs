71404a65bbac2dd247431010fcf329cf845f3409
Fabian-Lars <30730186+FabianLars@users.noreply.github.com>
2026-09-21 07:12:35 +0200

    Update typedoc 0.28 rebase (#4052)
    Co-authored-by: Tony <legendmastertony@gmail.com>

--- packages/js-api-generator/build.ts
+++ packages/js-api-generator/build.ts
@@ -1,23 +1,23 @@
 import {
   Application,
-  DeclarationReflection,
   Options,
   PageEvent,
   Reflection,
-  SignatureReflection,
   TSConfigReader,
+  type RouterTarget,
   type TypeDocOptions,
 } from 'typedoc';
 import {
   MarkdownPageEvent,
   MarkdownTheme,
   MarkdownThemeContext,
-  type MarkdownApplication,
   type PluginOptions,
 } from 'typedoc-plugin-markdown';
 import { existsSync, readFileSync, writeFileSync } from 'node:fs';
 
-const typeDocConfigBaseOptions: Partial<TypeDocOptions | PluginOptions> = {
+type ConfigOptions = Partial<Omit<TypeDocOptions, 'outputs'> & PluginOptions>;
+
+const typeDocConfigBaseOptions: ConfigOptions = {
   // TypeDoc options
   // https://typedoc.org/options/
   githubPages: false,
@@ -44,23 +44,6 @@ const typeDocConfigBaseOptions: Partial<TypeDocOptions | PluginOptions> = {
 };
 
 async function generator() {
-  if (existsSync('../tauri/packages/api/node_modules')) {
-    const coreJsOptions: Partial<TypeDocOptions> = {
-      entryPoints: ['../tauri/packages/api/src/index.ts'],
-      tsconfig: '../tauri/packages/api/tsconfig.json',
-      gitRevision: 'dev',
-      publicPath: '/reference/javascript/api/',
-      basePath: '/reference/javascript/api/',
-      ...typeDocConfigBaseOptions,
-    };
-
-    await generateDocs(coreJsOptions);
-  } else {
-    console.log(
-      'Tauri V2 submodule is not initialized, respective API routes will not be rendered.'
-    );
-  }
-
   const plugins = [
     'autostart',
     'barcode-scanner',
@@ -92,18 +75,10 @@ async function generator() {
   ];
 
   if (existsSync('../plugins-workspace/node_modules')) {
-    // TODO: Actually fix this
-    const data = readFileSync('../plugins-workspace/plugins/fs/guest-js/index.ts', {
-      encoding: 'utf8',
-    });
-    writeFileSync(
-      '../plugins-workspace/plugins/fs/guest-js/index.ts',
-      data.replace(/Uint8Array<ArrayBuffer>/g, 'Uint8Array'),
-      { encoding: 'utf8' }
-    );
+    let firstPlugin = true;
 
-    plugins.forEach(async (plugin) => {
-      const pluginJsOptions: Partial<TypeDocOptions> = {
+    for (const plugin of plugins) {
+      const pluginJsOptions: ConfigOptions = {
         entryPoints: [`../plugins-workspace/plugins/${plugin}/guest-js/index.ts`],
         tsconfig: `../plugins-workspace/plugins/${plugin}/tsconfig.json`,
         gitRevision: 'v2',
@@ -111,11 +86,14 @@ async function generator() {
         basePath: `/reference/javascript/`,
         ...typeDocConfigBaseOptions,
         // Must go after to override base
+        cleanOutputDir: firstPlugin,
         entryFileName: `${plugin}.md`,
       };
 
       await generateDocs(pluginJsOptions);
-    });
+
+      firstPlugin = false;
+    }
   } else {
     console.log(
       'Plugins workspace submodule is not initialized, respective API routes will not be rendered.'
@@ -123,7 +101,7 @@ async function generator() {
   }
 
   if (existsSync('../tauri/packages/api/node_modules')) {
-    const coreJsOptions: Partial<TypeDocOptions> = {
+    const coreJsOptions: ConfigOptions = {
       entryPoints: ['../tauri/packages/api/src/index.ts'],
       tsconfig: '../tauri/packages/api/tsconfig.json',
       gitRevision: 'dev',
@@ -141,15 +119,17 @@ async function generator() {
 }
 
 // Adapted from https://github.com/HiDeoo/starlight-typedoc
-async function generateDocs(options: Partial<TypeDocOptions>) {
+async function generateDocs(options: ConfigOptions) {
   const outputDir = `../../src/content/docs${options.publicPath}`;
 
-  const app = await Application.bootstrapWithPlugins(options);
+  const app = await Application.bootstrapWithPlugins({
+    ...options,
+    outputs: [{ name: 'markdown', path: outputDir }],
+  });
   app.options.addReader(new TSConfigReader());
-  // @ts-ignore
   app.renderer.defineTheme('tauri-theme', TauriTheme);
 
-  app.renderer.on(PageEvent.END, (event: PageEvent<DeclarationReflection>) => {
+  app.renderer.on(PageEvent.END, (event: PageEvent<RouterTarget>) => {
     pageEventEnd(event);
   });
 
@@ -158,12 +138,12 @@ async function generateDocs(options: Partial<TypeDocOptions>) {
   if (!project) {
     throw new Error(`typedoc conversion failed for ${options.entryPoints}, see errors above`);
   }
-  await app.generateDocs(project, outputDir);
+  await app.generateOutputs(project);
 }
 
 // Adds frontmatter to the top of the file
 // Adapted from https://github.com/HiDeoo/starlight-typedoc
-function pageEventEnd(event: PageEvent<DeclarationReflection>) {
+function pageEventEnd(event: PageEvent<RouterTarget>) {
   if (!event.contents) {
     return;
   }
@@ -189,7 +169,7 @@ class TauriThemeRenderContext extends MarkdownThemeContext {
     this.partials = {
       ...this.partials,
       // Formats `@source` to be a single line
-      sources: (model: DeclarationReflection | SignatureReflection, options: object) => {
+      sources: (model, options) => {
         if (!model.sources) {
           return '';
         }
@@ -232,16 +212,18 @@ class TauriThemeRenderContext extends MarkdownThemeContext {
   }
 
   // Adapted from https://github.com/HiDeoo/starlight-typedoc/blob/d95072e218004276942a5132ec8a4e3561425903/packages/starlight-typedoc/src/libs/theme.ts#L28
-  override getRelativeUrl = (url: string) => {
+  override urlTo(reflection: Reflection): string {
+    const url = super.urlTo(reflection);
+
     if (/^(http|ftp)s?:\/\//.test(url)) {
       return url;
     }
 
-    url = decodeURI(
-      super.getRelativeUrl(url).replaceAll('.md', '/').replaceAll('.', '').toLowerCase()
-    ).replaceAll('\\', '/');
-    return url;
-  };
+    return decodeURI(url.replaceAll('.md', '/').replaceAll('.', '').toLowerCase()).replaceAll(
+      '\\',
+      '/'
+    );
+  }
 }
 
 // Overrides and extensions based on https://github.com/tgreyuk/typedoc-plugin-markdown/blob/next/packages/typedoc-plugin-markdown/docs/usage/customizing.md
